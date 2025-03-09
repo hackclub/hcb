174cf73d13564547b74d2bfad19e5b4d1b9a6cdf
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-03-09 11:16:45 -0500

    rename job

--- app/jobs/g_suite_job/close_remedied_revocations.rb
+++ app/jobs/g_suite_job/close_remedied_revocations.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module GSuiteJob
-  class CloseRemediedRevocationsJob < ApplicationJob
+  class CloseRemediedRevocations < ApplicationJob
     queue_as :low
 
     def perform
