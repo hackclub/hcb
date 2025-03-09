b9a685ea8b1ac9fbc6bf6522be6b740d90a50c1a
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-03-09 11:55:19 -0500

    add additional checks in case we somehow created a revocation for an immune org, so we never transition to revoked for immune orgs

--- app/jobs/g_suite_job/transition_expired_revocations.rb
+++ app/jobs/g_suite_job/transition_expired_revocations.rb
@@ -6,7 +6,11 @@ module GSuiteJob
 
     def perform
       GSuite::Revocation.where("scheduled_at < ?", 12.hours.ago).find_each(batch_size: 100) do |revocation|
-        revocation.mark_pending_revocation!
+        if revocation.g_suite.immune_to_revocation?
+          revocation.destroy!
+          next
+        end
+        revocation.mark_revoked!
       end
     end
 
