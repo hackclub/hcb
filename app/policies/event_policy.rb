ec5360148c0251b61731301cf7af78badc2ddf0e
Manu G <manusvathgurudath@gmail.com>
2025-03-18 01:44:58 +0000

    Change name

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -7,7 +7,7 @@ class EventPolicy < ApplicationPolicy
 
   # Event homepage
   def show?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
   # Turbo frames for the event homepage (show)
@@ -24,69 +24,61 @@ class EventPolicy < ApplicationPolicy
   alias_method :ledger?, :transactions?
 
   def toggle_hidden?
-    user&.admin?
+    admin?
   end
 
   def new?
-    user&.admin?
+    admin?
   end
 
   def create?
-    user&.admin?
+    admin?
   end
 
   def balance_by_date?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
-  # NOTE(@lachlanjc): this is bad, I’m sorry.
-  # This is the StripeCardsController#shipping method when rendered on the event
-  # card overview page. This should be moved out of here.
   def shipping?
-    admin_or_user?
+    allowed_user?
   end
 
   def edit?
-    admin_or_user?
+    can_edit_or_manage?
   end
 
-  # pinning a transaction to an event
   def pin?
-    admin_or_user?
+    allowed_user?
   end
 
   def update?
-    admin_or_manager?
+    can_edit_or_manage?
   end
 
   alias remove_header_image? update?
-
   alias remove_background_image? update?
-
   alias remove_logo? update?
-
   alias enable_feature? update?
-
   alias disable_feature? update?
 
   def validate_slug?
-    admin_or_user?
+    allowed_user?
   end
 
   def destroy?
-    user&.admin? && record.demo_mode?
+    admin? && record.demo_mode?
   end
 
   def team?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
   def emburse_card_overview?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
   def card_overview?
-    (is_public || admin_or_user?) && record.approved? && record.plan.cards_enabled?
+    (is_public || allowed_user?) && record.approved? && record.plan.cards_enabled?
   end
 
   def new_stripe_card?
@@ -94,71 +86,71 @@ class EventPolicy < ApplicationPolicy
   end
 
   def create_stripe_card?
-    admin_or_user? && is_not_demo_mode?
+    allowed_user? && is_not_demo_mode?
   end
 
   def documentation?
-    admin_or_user? && record.plan.documentation_enabled?
+    allowed_user? && record.plan.documentation_enabled?
   end
 
   def statements?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
   def async_balance?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
   def create_transfer?
-    admin_or_manager? && !record.demo_mode?
+    can_edit_or_manage? && !record.demo_mode?
   end
 
   def new_transfer?
-    admin_or_manager? && !record.demo_mode?
+    can_edit_or_manage? && !record.demo_mode?
   end
 
   def g_suite_overview?
-    admin_or_user? && is_not_demo_mode? && record.plan.google_workspace_enabled?
+    allowed_user? && is_not_demo_mode? && record.plan.google_workspace_enabled?
   end
 
   def g_suite_create?
-    admin_or_manager? && is_not_demo_mode? && record.plan.google_workspace_enabled?
+    can_edit_or_manage? && is_not_demo_mode? && record.plan.google_workspace_enabled?
   end
 
   def g_suite_verify?
-    admin_or_user? && is_not_demo_mode? && record.plan.google_workspace_enabled?
+    allowed_user? && is_not_demo_mode? && record.plan.google_workspace_enabled?
   end
 
   def transfers?
-    (is_public || admin_or_user?) && record.plan.transfers_enabled?
+    (is_public || allowed_user?) && record.plan.transfers_enabled?
   end
 
   def promotions?
-    admin_or_user? && record.plan.promotions_enabled?
+    allowed_user? && record.plan.promotions_enabled?
   end
 
   def reimbursements_pending_review_icon?
-    is_public || admin_or_user?
+    is_public || allowed_user?
   end
 
   def reimbursements?
-    admin_or_user? && record.plan.reimbursements_enabled?
+    allowed_user? && record.plan.reimbursements_enabled?
   end
 
   def employees?
-    admin_or_user?
+    allowed_user?
   end
 
   def donation_overview?
-    (is_public || admin_or_user?) && record.approved? && record.plan.donations_enabled?
+    (is_public || allowed_user?) && record.approved? && record.plan.donations_enabled?
   end
 
   def account_number?
-    admin_or_manager? && record.plan.account_number_enabled?
+    can_edit_or_manage? && record.plan.account_number_enabled?
   end
 
   def toggle_event_tag?
-    user.admin?
+    admin?
   end
 
   def receive_grant?
@@ -166,48 +158,52 @@ class EventPolicy < ApplicationPolicy
   end
 
   def audit_log?
-    user.admin?
+    admin?
   end
 
   def termination?
-    user&.admin?
+    admin?
   end
 
   def can_invite_user?
-    admin_or_manager?
+    can_edit_or_manage?
   end
 
   def claim_point_of_contact?
-    user&.admin?
+    admin?
   end
 
   def activation_flow?
-    user&.admin? && record.demo_mode?
+    admin? && record.demo_mode?
   end
 
   def activate?
-    user&.admin? && record.demo_mode?
+    admin? && record.demo_mode?
   end
 
   private
 
-  def admin_or_user?
-    admin? || user?
+  def allowed_user?
+    admin? || manager? || normal_user?
   end
 
   def admin?
     user&.admin?
   end
 
-  def user?
-    record.users.include?(user)
-  end
-
   def manager?
     OrganizerPosition.find_by(user:, event: record)&.manager?
   end
 
-  def admin_or_manager?
+  def normal_user?
+    record.users.include?(user) && !reader?
+  end
+
+  def reader?
+    OrganizerPosition.find_by(user:, event: record)&.reader?
+  end
+
+  def can_edit_or_manage?
     admin? || manager?
   end
 
@@ -218,5 +214,4 @@ class EventPolicy < ApplicationPolicy
   def is_public
     record.is_public?
   end
-
 end
