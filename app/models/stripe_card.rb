a4cabfa577177194b337cefcce41b1040ff65e22
Sam Poder <sampoder@berkeley.edu>
2025-03-22 04:57:12 -0500

    Update app/models/stripe_card.rb

--- app/models/stripe_card.rb
+++ app/models/stripe_card.rb
@@ -430,11 +430,11 @@ class StripeCard < ApplicationRecord
     event_card_grants_today = event.card_grants.where(created_at: 1.day.ago..).count
 
     if user_cards_today > 20
-      errors.add(:base, "Your account has been rate-limited from creating new cards. For help, email hcb@hackclub.com.")
+      errors.add(:base, "Your account has been rate-limited from creating new cards. Please try again tomorrow; for help, email hcb@hackclub.com.")
     end
 
     if (event_cards_today - event_card_grants_today) > 20 # card grants don't count against the limit
-      errors.add(:base, "Your organization has been rate-limited from creating new cards. For help, email hcb@hackclub.com.")
+      errors.add(:base, "Your organization has been rate-limited from creating new cards. Please try again tomorrow; for help, email hcb@hackclub.com.")
     end
   end
 
