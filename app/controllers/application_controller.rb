205bdcc287e2abee9799fee68ff4c205dafdbf68
Sam Poder <sampoder@berkeley.edu>
2025-04-03 15:23:49 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::Base
 
   # Enable Rack::MiniProfiler for admins
   before_action do
-    if current_user&.is_admin?
+    if current_user&.admin?
       Rack::MiniProfiler.authorize_request
     end
   end
