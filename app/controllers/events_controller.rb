9b4de8124a486406db75078bab7d3bb5e816e934
Sam Poder <39828164+sampoder@users.noreply.github.com>
2025-04-06 22:07:58 -0700

    more features

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -945,6 +945,7 @@ class EventsController < ApplicationController
       :stripe_card_logo,
       :stripe_card_shipping_type,
       :plan,
+      :finanically_frozen,
       card_grant_setting_attributes: [
         :merchant_lock,
         :category_lock,
