0bb83eab21bf7e01c1d42637cd6c9bfd1d420a97
Samuel Fernandez <me@sfernandez.dev>
2025-04-12 02:45:10 +0000

    fix lint

--- spec/mailers/previews/event_mailer_preview.rb
+++ spec/mailers/previews/event_mailer_preview.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class EventMailerPreview < ActionMailer::Preview
   def monthly_donation_summary
     EventMailer.with(event: Event.not_demo_mode.first).monthly_donation_summary
