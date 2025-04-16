1646bdae9290a10e60d038e4103466bf37e99084
Gary Tou <gary@garytou.com>
2025-04-16 11:06:31 -0500

    make it work

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -1,10 +1,17 @@
 class RafflesController < ApplicationController
-  skip_after_action :verify_authorized, only: :create
+  skip_after_action :verify_authorized, only: [:new, :create]
 
   def new
   end
 
   def create
-    Raffle.create!(user: current_user, program: params[:program])
+    raffle = Raffle.new(user: current_user, program: params[:program])
+    if raffle.save
+      flash[:success] = "Raffle joined!"
+      redirect_to root_path
+    else
+      flash[:error] = raffle.errors.full_messages.to_sentence
+      redirect_to new_raffle_path
+    end
   end
 end
