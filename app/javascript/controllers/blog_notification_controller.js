048d664bf9a369781840b970a076a18617816fce
yodalightsabr <yoda@yodacode.xyz>
2025-04-25 18:51:51 -0500

    Add error handling to prevent crashes

--- app/javascript/controllers/blog_notification_controller.js
+++ app/javascript/controllers/blog_notification_controller.js
@@ -4,18 +4,21 @@ export default class extends Controller {
   static targets = ['badge']
 
   connect() {
-    console.log('BlogNotificationController connected')
     this.updateBadge()
   }
 
   async updateBadge() {
-    const { count } = await fetch('https://blog.hcb.hackclub.com', {
-      credentials: 'include',
-    }).then(res => res.json())
-
-    if (count < 1) return
-
-    this.badgeTarget.innerText = count
-    this.badgeTarget.classList.remove('hidden')
+    try {
+      const { count } = await fetch('https://blog.hcb.hackclub.com', {
+        credentials: 'include',
+      }).then(res => res.json())
+  
+      if (count < 1) return
+  
+      this.badgeTarget.innerText = count
+      this.badgeTarget.classList.remove('hidden')
+    } catch (error) {
+      console.error("Error fetching notification count", error)
+    }
   }
 }
