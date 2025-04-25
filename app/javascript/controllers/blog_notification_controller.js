c7df9fea2a439ed9c775fdb0c731829bcf58c1f9
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-04-25 16:55:12 -0700

    Lint again 🤞

--- app/javascript/controllers/blog_notification_controller.js
+++ app/javascript/controllers/blog_notification_controller.js
@@ -12,9 +12,9 @@ export default class extends Controller {
       const { count } = await fetch('https://blog.hcb.hackclub.com', {
         credentials: 'include',
       }).then(res => res.json())
-  
+
       if (count < 1) return
-  
+
       this.badgeTarget.innerText = count
       this.badgeTarget.classList.remove('hidden')
     } catch (error) {
