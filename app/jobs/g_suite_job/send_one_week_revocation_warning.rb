9d3b1d6ac5b468e795bd9748a40e226dbb5f35c0
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-04-29 22:49:19 -0400

    additional sanity checks

--- app/jobs/g_suite_job/send_one_week_revocation_warning.rb
+++ app/jobs/g_suite_job/send_one_week_revocation_warning.rb
@@ -6,6 +6,10 @@ module GSuiteJob
 
     def perform
       GSuite::Revocation.where("scheduled_at < ?", 1.week.from_now).where(one_week_notice_sent: false).find_each(batch_size: 100) do |revocation|
+        if revocation.g_suite.immune_to_revocation?
+          revocation.destroy!
+          next
+        end
         GSuiteMailer.with(revocation: revocation).revocation_one_week_warning.deliver_later
         revocation.update!(one_week_notice_sent: true)
       end
