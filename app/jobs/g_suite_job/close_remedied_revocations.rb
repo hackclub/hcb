2c374507751fb9c0681a31c9cb47eb78a1c63b87
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-04-29 21:24:16 -0400

    consolidate logic

--- app/jobs/g_suite_job/close_remedied_revocations.rb
+++ app/jobs/g_suite_job/close_remedied_revocations.rb
@@ -6,9 +6,8 @@ module GSuiteJob
 
     def perform
       GSuite::Revocation.find_each(batch_size: 100) do |revocation|
-        if revocation.because_of_invalid_dns? && revocation.g_suite.verified?
-          revocation.destroy!
-        elsif revocation.because_of_accounts_inactive? && !revocation.g_suite.accounts_inactive?
+        if revocation.because_of_invalid_dns? && revocation.g_suite.verified? ||
+           revocation.because_of_accounts_inactive? && !revocation.g_suite.accounts_inactive?
           revocation.destroy!
         end
       end
