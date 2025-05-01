6b1d32a9b0d28a4eac01d010f7e1aa4c9af7ba74
Samuel Fernandez <me@sfernandez.dev>
2025-05-01 01:45:35 +0000

    Create job to send correction donation summary emails

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -13,7 +13,7 @@ class EventMailer < ApplicationMailer
 
     @total = @donations.sum(:amount)
 
-    mail to: @emails, subject: "#{@event.name} received #{@donations.length} donations this past month"
+    mail to: @emails, subject: "#{"[CORRECTION] " if params[:correction]} #{@event.name} received #{@donations.length} donations this past month"
   end
 
   private
