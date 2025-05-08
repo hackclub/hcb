fbf291ecf983e4ffb58d20e73d54fecd66debb83
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-05-08 19:30:46 +0000

    Fix contract bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -764,7 +764,7 @@ class Event < ApplicationRecord
   end
 
   def contract_signed
-    return if organizer_position_contracts.signed.any? || organizer_position_contracts.none?
+    return if organizer_position_contracts.signed.any? || organizer_position_contracts.none? || plan.is_a?(Event::Plan::HackClubAffiliate)
 
     errors.add(:base, "Missing a contract signee, non-demo mode organizations must have a contract signee.")
   end
