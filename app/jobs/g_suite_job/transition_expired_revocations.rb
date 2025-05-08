fe8c78520d587ccebb2ab420b4bc67a13756a787
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-05-07 22:10:39 -0400

    add comment

--- app/jobs/g_suite_job/transition_expired_revocations.rb
+++ app/jobs/g_suite_job/transition_expired_revocations.rb
@@ -5,7 +5,7 @@ module GSuiteJob
     queue_as :low
 
     def perform
-      GSuite::Revocation.where("scheduled_at < ?", 12.hours.ago).pending.find_each(batch_size: 100) do |revocation|
+      GSuite::Revocation.where("scheduled_at < ?", 12.hours.ago).pending.find_each(batch_size: 100) do |revocation| # we wait for 12 hours to allow for time zone differences
         if revocation.g_suite.immune_to_revocation?
           revocation.destroy!
           next
