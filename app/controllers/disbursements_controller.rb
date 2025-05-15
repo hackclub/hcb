32f8d7ed05d94632ee718b4c4bb98fd1009e4175
yodalightsabr <yoda@yodacode.xyz>
2025-05-15 16:55:56 -0500

    Update app/controllers/disbursements_controller.rb
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/controllers/disbursements_controller.rb
+++ app/controllers/disbursements_controller.rb
@@ -57,7 +57,7 @@ class DisbursementsController < ApplicationController
                                   elsif @source_event&.plan&.unrestricted_disbursements_enabled?
                                     Event.select("name, id, demo_mode, can_front_balance, slug").transparent.indexable.includes(:plan)
                                   else
-                                    current_user.events.not_hidden.without(@source_event).filter_demo_mode(false)
+                                    current_user.events.not_hidden.filter_demo_mode(false)
                                   end.sort_by { |e| user_event_ids.index(e.id) || Float::INFINITY }
 
     authorize @disbursement
