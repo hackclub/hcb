8ce3fe27ff650e1a6f3046db3b2b9eb85cb957e0
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-05-19 23:54:41 -0700

    one last schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -27,10 +29,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_19_235132) do
     t.datetime "updated_at", precision: nil, null: false
     t.string "recipient_tel"
     t.datetime "rejected_at", precision: nil
+    t.datetime "scheduled_arrival_date", precision: nil
     t.text "payment_for"
     t.string "aasm_state"
     t.text "confirmation_number"
-    t.datetime "scheduled_arrival_date", precision: nil
     t.text "account_number_ciphertext"
     t.bigint "processor_id"
     t.text "increase_id"
