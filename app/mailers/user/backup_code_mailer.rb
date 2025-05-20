c7526f3787cff3152ee38b487bc8f6d2cb329e60
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-05-19 23:41:13 -0700

    rename few codes left mailer

--- app/mailers/user/backup_code_mailer.rb
+++ app/mailers/user/backup_code_mailer.rb
@@ -22,7 +22,7 @@ class User
       mail subject: "HCB backup codes are disabled"
     end
 
-    def two_or_fewer_codes_left
+    def three_or_fewer_codes_left
       mail subject: "[Action Requested] You've almost used all your backup codes for HCB"
     end
 
