9fb1eb9cd1019047de17438695eb3842bd4d378a
Samuel Fernandez <me@sfernandez.dev>
2025-05-22 02:38:45 +0000

    Fix amount difference math

--- app/services/spending_control_service.rb
+++ app/services/spending_control_service.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module SpendingControlService
-  def self.check_low_balance(spending_control, amount)
-    historic_max_balance_cents = (spending_control.balance_cents - amount) / 10
+  def self.check_low_balance(spending_control, amount_difference)
+    historic_max_balance_cents = (spending_control.balance_cents - amount_difference) / 10
     threshold = [historic_max_balance_cents, 25_00].max
 
     if spending_control.balance_cents <= threshold
