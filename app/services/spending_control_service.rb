a4e7fae72ea277ed224d4c54d231dd326672435a
Samuel Fernandez <me@sfernandez.dev>
2025-05-22 02:50:54 +0000

    Only use $25 threshold on first time

--- app/services/spending_control_service.rb
+++ app/services/spending_control_service.rb
@@ -2,12 +2,12 @@
 
 module SpendingControlService
   def self.check_low_balance(spending_control, amount_difference)
-    historic_max_balance_cents = (spending_control.balance_cents - amount_difference) / 10
-    threshold = [historic_max_balance_cents, 25_00].max
+    historic_max_balance_cents = spending_control.balance_cents - amount_difference
 
-    if spending_control.balance_cents <= threshold
+    # These emails could get repetitive quickly if a user has a low spending balance and is only spending small amounts
+    # So the $25 threshold is only taken into account when crossed for the first time
+    if (historic_max_balance_cents > 25_00 && spending_control.balance_cents <= 25_00) || spending_control.balance_cents <= historic_max_balance_cents / 10
       OrganizerPosition::Spending::ControlsMailer.with(control: spending_control).warning.deliver_later
     end
-
   end
 end
