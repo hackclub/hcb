5d1a823db19ef2b55eb5fd8c01e356cb9cefc086
Samuel Fernandez <me@sfernandez.dev>
2025-05-22 17:47:39 +0000

    Move warning url to inside view

--- app/mailers/organizer_position/spending/controls_mailer.rb
+++ app/mailers/organizer_position/spending/controls_mailer.rb
@@ -5,7 +5,6 @@ class OrganizerPosition
     class ControlsMailer < ApplicationMailer
       def warning
         @control = params[:control]
-        @controls_path = Rails.application.routes.url_helpers.event_organizer_position_spending_controls_path(event_id: @control.organizer_position.event, organizer_position_id: @control.organizer_position)
 
         mail to: @control.organizer_position.user.email_address_with_name, subject: "Your spending balance on #{@control.organizer_position.event.name} is getting low"
       end
