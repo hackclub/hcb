fa48b164c6fc27442b1236aa36eb1bdef6488238
Mahad Kalam <mahadkalam1@proton.me>
2025-05-23 14:39:33 +0100

    Add 24 hour and 6 hour card grant expiry warnings

--- app/jobs/card_grant/expiration_job.rb
+++ app/jobs/card_grant/expiration_job.rb
@@ -8,16 +8,21 @@ class CardGrant
         card_grant.expire!
       end
 
+      CardGrant.active.expires_on(6.hours.from_now).find_each do |card_grant|
+        CardGrantMailer.with(card_grant:, expiry_time: "6 hours").card_grant_expiry_notification.deliver_later
+      end
+
+      CardGrant.active.expires_on(1.day.from_now).find_each do |card_grant|
+        CardGrantMailer.with(card_grant:, expiry_time: "24 hours").card_grant_expiry_notification.deliver_later
+      end
+
       CardGrant.active.expires_on(3.days.from_now).find_each do |card_grant|
-        CardGrantMailer.with(card_grant:, expiry_time: "3 days").card_grant_days_expire_notification.deliver_later
+        CardGrantMailer.with(card_grant:, expiry_time: "3 days").card_grant_expiry_notification.deliver_later
       end
 
       CardGrant.active.expires_on(1.month.from_now).find_each do |card_grant|
-        CardGrantMailer.with(card_grant:, expiry_time: "1 month").card_grant_month_expire_notification.deliver_later
+        CardGrantMailer.with(card_grant:, expiry_time: "1 month").card_grant_expiry_notification.deliver_later
       end
-
     end
-
   end
-
 end
