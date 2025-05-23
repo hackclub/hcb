c05d401325caa9b72ca1c78af2a764464d89806b
Mahad Kalam <mahadkalam1@proton.me>
2025-05-23 14:44:08 +0100

    Fix card grant expiration notifications

--- app/jobs/card_grant/expiration_job.rb
+++ app/jobs/card_grant/expiration_job.rb
@@ -4,23 +4,20 @@ class CardGrant
   class ExpirationJob < ApplicationJob
     queue_as :low
     def perform
+      # Expire grants that have passed their expiration date
       CardGrant.active.expired_before(Time.now).find_each do |card_grant|
         card_grant.expire!
       end
 
-      CardGrant.active.expires_on(6.hours.from_now).find_each do |card_grant|
-        CardGrantMailer.with(card_grant:, expiry_time: "6 hours").card_grant_expiry_notification.deliver_later
-      end
-
-      CardGrant.active.expires_on(1.day.from_now).find_each do |card_grant|
+      CardGrant.active.expires_between(Time.now, 24.hours.from_now).find_each do |card_grant|
         CardGrantMailer.with(card_grant:, expiry_time: "24 hours").card_grant_expiry_notification.deliver_later
       end
 
-      CardGrant.active.expires_on(3.days.from_now).find_each do |card_grant|
+      CardGrant.active.expires_between(Time.now, 3.days.from_now).find_each do |card_grant|
         CardGrantMailer.with(card_grant:, expiry_time: "3 days").card_grant_expiry_notification.deliver_later
       end
 
-      CardGrant.active.expires_on(1.month.from_now).find_each do |card_grant|
+      CardGrant.active.expires_between(Time.now, 1.month.from_now).find_each do |card_grant|
         CardGrantMailer.with(card_grant:, expiry_time: "1 month").card_grant_expiry_notification.deliver_later
       end
     end
