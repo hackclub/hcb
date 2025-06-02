be2dcd188fd2b62915d9943e903d640f154f33ee
Samuel Fernandez <me@sfernandez.dev>
2025-06-02 19:44:04 +0000

    Handle rate limiting in donation summary sending

--- app/jobs/monthly_donation_summary_job.rb
+++ app/jobs/monthly_donation_summary_job.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class MonthlyDonationSummaryJob < ApplicationJob
-  queue_as :default
+  queue_as :bulk_email
   def perform
     Event.includes(:donations).where("donations.created_at > ?", 1.month.ago).references(:donations).find_each do |event|
       mailer = EventMailer.with(event: event)
-      mailer.monthly_donation_summary.deliver_later
+
+      attempt = 1
+      mail = nil
+      while mail.nil?
+        begin
+          mail = mailer.monthly_donation_summary.deliver_now
+        rescue Net::SMTPServerBusy
+          # Rate limited by SES
+          sleep 0.5 * attempt
+          attempt += 1
+        end
+      end
     end
   end
 
