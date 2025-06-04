9a47c0f0d6ea2460fd8e60ac25e80396d059fa15
Sam Poder <sampoder@berkeley.edu>
2025-06-03 21:33:17 -0500

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_02_201214) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_03_011828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
