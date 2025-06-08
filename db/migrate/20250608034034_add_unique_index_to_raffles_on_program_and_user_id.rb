08818348eabccd0db674da16c6426762856354d3
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-06-08 19:13:53 +0000

    Migrate schema

--- db/migrate/20250608034034_add_unique_index_to_raffles_on_program_and_user_id.rb
+++ db/migrate/20250608034034_add_unique_index_to_raffles_on_program_and_user_id.rb
@@ -2,6 +2,8 @@ class AddUniqueIndexToRafflesOnProgramAndUserId < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
 
   def change
-    add_index :raffles, [:program, :user_id], unique: true, algorithm: :concurrently
+    remove_index :raffles, name: :index_raffles_on_program_and_user_id, if_exists: true
+
+    add_index :raffles, [:program, :user_id], unique: true, algorithm: :concurrently, name: :index_raffles_on_program_and_user_id
   end
 end
