a001fe1bea12824897d4b37d6ff97ed6a09a81c6
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-06-12 14:21:46 -0700

    use retry_on for deletedomainusersjob

--- app/jobs/g_suite/delete_domain_users_job.rb
+++ app/jobs/g_suite/delete_domain_users_job.rb
@@ -6,23 +6,19 @@ class GSuite
 
     include Partners::Google::GSuite::Shared::DirectoryClient
 
-    def perform(domain:, remote_org_unit_path:, times_queued: 1)
+    retry_on Google::Apis::ClientError, attempts: 3, wait: 2.minutes do |job, e|
+      Rails.error.report("Failed to delete GSuite domain #{domain}'s users after 3 attempts (pls do manually): #{e.message}\nBacktrace: #{e.backtrace}")
+    end
+
+    def perform(domain:, remote_org_unit_path:)
       unless Rails.env.production?
         puts "☣️ In production, we would currently be deleting the domain #{domain}'s users on Google Admin ☣️"
         DeleteOrgUnitJob.set(wait: 1.minute).perform_later(domain:, remote_org_unit_path:)
         return
       end
 
-      begin
-        Partners::Google::GSuite::DeleteUsersOnDomain.new(domain:).run
-        DeleteOrgUnitJob.set(wait: 1.minute).perform_later(domain:, remote_org_unit_path:)
-      rescue => e
-        if times_queued < 3
-          retry_job(wait: 2.minutes, domain:, remote_org_unit_path:, times_queued: times_queued + 1)
-          return
-        end
-        Rails.error.report("Failed to delete GSuite domain #{domain}'s users after 3 attempts (pls do manually): #{e.message}\nBacktrace: #{e.backtrace}")
-      end
+      Partners::Google::GSuite::DeleteUsersOnDomain.new(domain:).run
+      DeleteOrgUnitJob.set(wait: 1.minute).perform_later(domain:, remote_org_unit_path:)
     end
 
   end
