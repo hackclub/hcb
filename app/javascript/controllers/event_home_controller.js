c93f638580bbfe986ad5d2e27073d8452a917ba6
Samuel Fernandez <me@sfernandez.dev>
2025-06-23 14:02:49 -0400

    Fix formatting
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/javascript/controllers/event_home_controller.js
+++ app/javascript/controllers/event_home_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   static targets = ['merchants', 'categories', 'tags', 'users', 'button']
 
   updateTimeframe(event) {
-    const timeframe = event.target.innerText;
+    const timeframe = event.target.innerText
 
-    this.buttonTarget.innerText = timeframe;
-    this.merchantsTarget.src = `${this.merchantsTarget.src.split("?")[0]}?timeframe=${timeframe}`;
-    this.categoriesTarget.src = `${this.categoriesTarget.src.split("?")[0]}?timeframe=${timeframe}`;
-    this.tagsTarget.src = `${this.tagsTarget.src.split("?")[0]}?timeframe=${timeframe}`;
-    this.usersTarget.src = `${this.usersTarget.src.split("?")[0]}?timeframe=${timeframe}`;
+    this.buttonTarget.innerText = timeframe
+    this.merchantsTarget.src = `${this.merchantsTarget.src.split('?')[0]}?timeframe=${timeframe}`
+    this.categoriesTarget.src = `${this.categoriesTarget.src.split('?')[0]}?timeframe=${timeframe}`
+    this.tagsTarget.src = `${this.tagsTarget.src.split('?')[0]}?timeframe=${timeframe}`
+    this.usersTarget.src = `${this.usersTarget.src.split('?')[0]}?timeframe=${timeframe}`
   }
 }
