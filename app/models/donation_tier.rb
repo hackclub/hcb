d30cb0e5b52136a471ef5d439e116b726671039d
Manu G <manusvathgurudath@gmail.com>
2025-06-26 03:52:24 +0000

    Update schema information comments in DonationTier model

--- app/models/donation_tier.rb
+++ app/models/donation_tier.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: donation_tiers
+#
+#  id           :bigint           not null, primary key
+#  amount_cents :integer          not null
+#  deleted_at   :datetime
+#  description  :text
+#  image_url    :string
+#  name         :string           not null
+#  position     :integer          default(0), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  event_id     :bigint           not null
+#
+# Indexes
+#
+#  index_donation_tiers_on_event_id  (event_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_id => events.id)
+#
 class DonationTier < ApplicationRecord
   belongs_to :event
 
