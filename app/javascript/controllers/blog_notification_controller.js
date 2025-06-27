4745ba0bae79b945933b971cdd2394ccbe49e4c3
Samuel Fernandez <me@sfernandez.dev>
2025-06-27 19:37:58 +0000

    Remove unused stimulus controller logic

--- app/javascript/controllers/blog_notification_controller.js
+++ app/javascript/controllers/blog_notification_controller.js
@@ -1,17 +1,10 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['badge', 'widget']
-
-  open = false
+  static targets = ['badge']
 
   connect() {
     this.updateBadge()
-    document.addEventListener('click', this.handleDocumentClick.bind(this))
-  }
-
-  disconnect() {
-    document.removeEventListener('click', this.handleDocumentClick.bind(this))
   }
 
   async updateBadge() {
@@ -29,41 +22,6 @@ export default class extends Controller {
       this.badgeTarget.classList.remove('hidden')
     } catch (error) {
       console.error('Error fetching notification count', error)
-    }
-  }
-
-  toggleWidget(event) {
-    event.preventDefault();
-    event.stopPropagation()
-
-    if (this.open) {
-      this.closeWidget()
-    } else {
-      this.openWidget()
-    }
-  }
-
-  openWidget() {
-    this.widgetTarget.classList.remove("fade-card-hide")
-    this.widgetTarget.classList.add("fade-card-show")
-    
-    this.open = true;
-  }
-
-  closeWidget() {
-    this.widgetTarget.classList.remove("fade-card-show")
-    this.widgetTarget.classList.add("fade-card-hide")
-
-    this.open = false;
-  }
-
-  keydown(e) {
-    if (e.code == 'Escape' && this.open) this.closeWidget()
-  }
-
-  handleDocumentClick(event) {
-    if (!this.widgetTarget.contains(event.target) && this.open == true) {
-      this.closeWidget()
     }
   }
 }
