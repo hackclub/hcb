8cf95e9a4c79c2919b79bbf59dbd589e912b769e
Manu G <manusvathgurudath@gmail.com>
2025-06-27 07:08:51 +0000

    Add job to clear old user sessions daily at 3 AM

---
+++ app/jobs/user/clear_old_user_sessions_job.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class User
+  class ClearOldUserSessionsJob < ApplicationJob
+    queue_as :low
+    
+    def perform
+      UserSession.where("created_at < ?", 1.year.ago).find_each do |session|
+        session.update_columns(
+          device_info: nil,
+          os_info: nil,
+          timezone: nil,
+          ip: nil,
+          impersonated_by_id: nil,
+          latitude: nil,
+          longitude: nil
+        )
+      end
+    end
+  end
+end
