36668c3190250a3e9d296b516d755c04545063b8
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-06-27 10:06:04 -0400

    Update app/jobs/weekly_ysws_event_summary_job.rb
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/jobs/weekly_ysws_event_summary_job.rb
+++ app/jobs/weekly_ysws_event_summary_job.rb
@@ -4,10 +4,9 @@ class WeeklyYswsEventSummaryJob < ApplicationJob
   queue_as :default
   def perform
     events = Event.ysws.where("created_at > ?", 7.days.ago)
-    if events.length > 0
-      mailer = AdminMailer.with(events:)
-      mailer.weekly_ysws_event_summary.deliver_later
-    end
+    return if events.none?
+
+    AdminMailer.with(events:).weekly_ysws_event_summary.deliver_later
   end
 
 end
