c39677a769e7b002d57bcca6c6572db0fbeab7c4
Sam Poder <sampoder@berkeley.edu>
2025-07-01 19:22:40 -0500

    Fix migration date

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_24_191949) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_30_174849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
