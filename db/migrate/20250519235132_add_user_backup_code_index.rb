864e4f90b8a86943cefb8493ea391f6bf683fa38
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-07-03 14:01:56 -0700

    add updated index

--- db/migrate/20250519235132_add_user_backup_code_index.rb
+++ db/migrate/20250519235132_add_user_backup_code_index.rb
@@ -2,6 +2,6 @@ class AddUserBackupCodeIndex < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
 
   def change
-    add_index :user_backup_codes, :code_hash, unique: true, algorithm: :concurrently
+    add_index :user_backup_codes, :code_digest, unique: true, algorithm: :concurrently
   end
 end
