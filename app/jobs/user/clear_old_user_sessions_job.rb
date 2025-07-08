ac90bc6679bff24570885f02e659dd2421103480
Manu G <manusvathgurudath@gmail.com>
2025-07-08 03:03:45 +0000

    Remove unused session attributes from ClearOldUserSessionsJob

--- app/jobs/user/clear_old_user_sessions_job.rb
+++ app/jobs/user/clear_old_user_sessions_job.rb
@@ -8,10 +8,6 @@ class UserSession
       UserSession.where("created_at < ?", 1.year.ago).find_each do |session|
         session.update_columns(
           device_info: nil,
-          os_info: nil,
-          timezone: nil,
-          ip: nil,
-          impersonated_by_id: nil,
           latitude: nil,
           longitude: nil
         )
