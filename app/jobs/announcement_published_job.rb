938a61abac031d51aa2b3ea27cf1833c11677bfb
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-07-08 16:35:02 -0400

    Update app/jobs/announcement_published_job.rb
    Co-authored-by: David Cornu <davidc@hackclub.com>

--- app/jobs/announcement_published_job.rb
+++ app/jobs/announcement_published_job.rb
@@ -3,10 +3,11 @@
 class AnnouncementPublishedJob < ApplicationJob
   queue_as :default
   def perform(announcement:)
-    emails = announcement.event.followers.map(&:email_address_with_name)
-
-    emails.each do |email|
-      AnnouncementMailer.with(announcement:, email:).announcement_published.deliver_later
+    announcement.event.followers.find_each do |follower|
+      AnnouncementMailer.with(
+        announcement:, 
+        email: follower.email_address_with_name
+      ).announcement_published.deliver_later
     end
   end
 
