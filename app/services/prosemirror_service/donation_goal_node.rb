01700e10955bb813771280c4fe169661c9ba6239
Samuel Fernandez <me@sfernandez.dev>
2025-07-09 18:54:31 +0000

    Remove extra margin from donation goal node

--- app/services/prosemirror_service/donation_goal_node.rb
+++ app/services/prosemirror_service/donation_goal_node.rb
@@ -20,7 +20,7 @@ module ProsemirrorService
       percentage = goal.progress_amount_cents.to_f / goal.amount_cents
       <<-HTML.chomp
         <div class="donationGoal flex flex-col">
-          <p class="text-center italic"><span class="font-bold">#{render_money goal.progress_amount_cents}</span> raised of <span class="font-bold">#{render_money goal.amount_cents}</span> goal </p>
+          <p class="text-center italic mt-0"><span class="font-bold">#{render_money goal.progress_amount_cents}</span> raised of <span class="font-bold">#{render_money goal.amount_cents}</span> goal </p>
           <div class="bg-gray-200 dark:bg-neutral-700 rounded-full w-full h-4">
             <div class="h-full bg-primary rounded flex items-center justify-center" style="width: #{percentage * 100}%">
               <p class="text-sm text-black">#{number_with_precision(percentage * 100, precision: 1)}%</p>
