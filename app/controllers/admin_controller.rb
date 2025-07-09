9ad363d109e21746df951805fe24975c11929cfe
Sarvesh <78450215+Sarvesh-Mk@users.noreply.github.com>
2025-07-09 15:59:15 +0000

    Fixed linting and syntax

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -105,7 +105,7 @@ class AdminController < ApplicationController
       country: country&.alpha2,
       point_of_contact_id: current_user.id,
       approved: true,
-      tags: [EventTag::Tags::ALL.ORGANIZED_BY_TEENAGERS] if application['TEEN'],
+      tags: application["TEEN"] ? [EventTag::Tags::ORGANIZED_BY_TEENAGERS] : [],
       demo_mode: true
     ).run
 
