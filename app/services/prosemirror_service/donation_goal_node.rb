b55fae213271d1050c2d7cda66b64a9857a99e20
Samuel Fernandez <me@sfernandez.dev>
2025-07-09 18:48:57 +0000

    Add back donation goal html

--- app/services/prosemirror_service/donation_goal_node.rb
+++ app/services/prosemirror_service/donation_goal_node.rb
@@ -8,7 +8,7 @@ module ProsemirrorService
     @tag_name = "div"
 
     def tag
-      [{ tag: self.class.tag_name, attrs: (@node.attrs || {}).merge({ class: "donationGoal flex flex-col" }) }]
+      [{ tag: self.class.tag_name, attrs: (@node.attrs || {}).merge({ class: "donationGoal flex flex-col py-2" }) }]
     end
 
     def matching
@@ -19,7 +19,13 @@ module ProsemirrorService
       goal = ProsemirrorService::Renderer.event.donation_goal
       percentage = goal.progress_amount_cents.to_f / goal.amount_cents
       <<-HTML.chomp
-        <div class="">
+        <div class="donationGoal flex flex-col">
+          <p class="text-center italic"><span class="font-bold">#{render_money goal.progress_amount_cents}</span> raised of <span class="font-bold">#{render_money goal.amount_cents}</span> goal </p>
+          <div class="bg-gray-200 dark:bg-neutral-700 rounded-full w-full h-4">
+            <div class="h-full bg-primary rounded flex items-center justify-center" style="width: #{percentage * 100}%">
+              <p class="text-sm text-black">#{number_with_precision(percentage * 100, precision: 1)}%</p>
+            </div>
+          </div>
         </div>
       HTML
     end
