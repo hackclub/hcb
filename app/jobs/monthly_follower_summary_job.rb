4e3093a737828fdddbd556a226319a1467c13d78
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-07-11 10:40:07 -0400

    Update app/jobs/monthly_follower_summary_job.rb
    Co-authored-by: David Cornu <davidc@hackclub.com>

--- app/jobs/monthly_follower_summary_job.rb
+++ app/jobs/monthly_follower_summary_job.rb
@@ -3,7 +3,7 @@
 class MonthlyFollowerSummaryJob < ApplicationJob
   queue_as :default
   def perform
-    Event.includes(:event_follows).where("event_follows.created_at > ?", Time.now.last_month.beginning_of_month..).references(:event_follows).find_each do |event|
+    Event.includes(:event_follows).where(event_follows: { created_at: Time.now.last_month.beginning_of_month.. }).find_each do |event|
       mailer = EventMailer.with(event: event)
       mailer.monthly_follower_summary.deliver_later
     end
