2d7dc451c404189456f565de62a612590b96347c
Samuel Fernandez <me@sfernandez.dev>
2025-07-16 17:08:30 +0000

    Add job and cron to delete old template draft announcements

---
+++ app/jobs/delete_old_template_drafts_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class DeleteOldTemplateDraftsJob < ApplicationJob
+  queue_as :low
+
+  def perform
+    old_template_drafts = Announcement.template_draft.where("created_at < ?", 1.month.ago)
+    old_template_drafts.destroy_all
+  end
+
+end
