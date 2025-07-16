dce1abb94cb8d6e3adf538707c72d5c4776837d7
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-07-16 09:48:08 -0400

    Remove default color

--- db/migrate/20250715203909_add_background_image_url_and_login_text_to_referral_program.rb
+++ db/migrate/20250715203909_add_background_image_url_and_login_text_to_referral_program.rb
@@ -3,6 +3,6 @@ class AddBackgroundImageUrlAndLoginTextToReferralProgram < ActiveRecord::Migrati
     add_column :referral_programs, :background_image_url, :string
     add_column :referral_programs, :login_header_text, :string
     add_column :referral_programs, :login_body_text, :text
-    add_column :referral_programs, :login_text_color, :string, default: "#ffffff"
+    add_column :referral_programs, :login_text_color, :string
   end
 end
