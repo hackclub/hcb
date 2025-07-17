3a2b337b6dc7a73b9ea64c984f0bd7d07a46ac53
Gary Tou <gary@garytou.com>
2025-07-17 10:13:52 -0400

    Handle errors from publishing announcement

--- app/jobs/announcement/monthly_job.rb
+++ app/jobs/announcement/monthly_job.rb
@@ -3,9 +3,11 @@
 class Announcement
   class MonthlyJob < ApplicationJob
     queue_as :default
-    def perform_later
+    def perform
       Announcement.monthly.where("announcements.created_at >= ?", Date.today.prev_month.beginning_of_month).find_each do |announcement|
-        announcement.publish!
+        Rails.error.handle do
+          announcement.publish!
+        end
       end
 
       Event.transparent.each do |event|
