481bf5219d064d87f62c8d26a8fa17c8a39738d4
Samuel Fernandez <me@sfernandez.dev>
2025-07-18 20:33:23 +0000

    Change from 1 month to 2 months

--- app/jobs/delete_old_template_drafts_job.rb
+++ app/jobs/delete_old_template_drafts_job.rb
@@ -4,7 +4,7 @@ class DeleteOldTemplateDraftsJob < ApplicationJob
   queue_as :low
 
   def perform
-    old_template_drafts = Announcement.template_draft.where("created_at < ?", 1.month.ago)
+    old_template_drafts = Announcement.template_draft.where("created_at < ?", 2.months.ago)
     old_template_drafts.delete_all!
   end
 
