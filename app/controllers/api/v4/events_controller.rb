237e80f2dcbc427f8d78f0b3ed73233e1dc5a1cd
24c02 <163450896+24c02@users.noreply.github.com>
2025-07-22 12:36:12 -0400

    [v4] factor @​​​event into set_event

--- app/controllers/api/v4/events_controller.rb
+++ app/controllers/api/v4/events_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V4
     class EventsController < ApplicationController
+      before_action :set_event, except: [:index]
       skip_after_action :verify_authorized, only: [:index]
 
       def index
@@ -10,11 +11,10 @@ module Api
       end
 
       def show
-        @event = authorize Event.find_by_public_id(params[:id]) || Event.friendly.find(params[:id])
+        authorize @event
       end
 
       def transactions
-        @event = Event.find_by_public_id(params[:id]) || Event.friendly.find(params[:id])
         authorize @event, :show?
 
         @settled_transactions = TransactionGroupingEngine::Transaction::All.new(event_id: @event.id).run
@@ -32,6 +32,10 @@ module Api
       end
 
       private
+
+      def set_event
+        @event = Event.find_by_public_id(params[:id]) || Event.friendly.find(params[:id])
+      end
 
       def paginate_transactions(transactions)
         limit = params[:limit]&.to_i || 25
