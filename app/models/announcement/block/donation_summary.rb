58c5e3322e62b732a1f94e7d7cef089029ffd3bd
Samuel Fernandez <me@sfernandez.dev>
2025-07-22 13:59:54 +0000

    Add end date and fallbacks to donation summary block

--- app/models/announcement/block/donation_summary.rb
+++ app/models/announcement/block/donation_summary.rb
@@ -24,12 +24,30 @@
 class Announcement
   class Block
     class DonationSummary < ::Announcement::Block
+      before_create do
+        start_date_param
+        end_date_param
+
+        self
+      end
+
       def render_html(is_email: false)
-        start_date = parameters["start_date"].present? ? Date.parse(parameters["start_date"]) : 1.month.ago
-        donations = announcement.event.donations.where(aasm_state: [:in_transit, :deposited], created_at: start_date..).order(:created_at)
+        start_date = start_date_param
+        end_date = end_date_param
+        donations = announcement.event.donations.where(aasm_state: [:in_transit, :deposited], created_at: start_date..end_date).order(:created_at)
         total = donations.sum(:amount)
 
-        Announcements::BlocksController.renderer.render partial: "announcements/blocks/donation_summary", locals: { donations:, total:, start_date:, is_email:, block: self }
+        Announcements::BlocksController.renderer.render partial: "announcements/blocks/donation_summary", locals: { donations:, total:, start_date:, end_date:, is_email:, block: self }
+      end
+
+      private
+
+      def start_date_param
+        self.parameters["start_date"] ||= 1.month.ago
+      end
+
+      def end_date_param
+        self.parameters["end_date"] ||= Time.now
       end
 
     end
