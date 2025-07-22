035c80dd709b89d1c9760d7363864fe457f89fb3
brarnaudovski <brarnaudovski@gmail.com>
2025-07-22 19:13:16 +0200

    stub web request from `ColumnService`

--- spec/controllers/ach_transfers_controller_spec.rb
+++ spec/controllers/ach_transfers_controller_spec.rb
@@ -3,6 +3,8 @@
 require "rails_helper"
 
 describe AchTransfersController do
+  include SessionSupport
+
   describe "show" do
     it "redirects to hcb code" do
       event = create(:event)
@@ -15,27 +17,72 @@ describe AchTransfersController do
 
   describe "validate_routing_number" do
     before do
-      # janky way of signing in for tests, we basically override the call in sessions_helper that checks for current_user to return this test user
       user = create(:user)
-      allow(controller).to receive(:current_user).and_return(user)
+      sign_in(user)
     end
 
-    it "doesn't perform a lookup when routing number is invalid" do
-      expect(ColumnService).not_to receive(:get)
+    context "when value param is empty" do
+      it "doesn't perform a lookup and is valid" do
+        get :validate_routing_number, params: { value: "" }
 
-      get :validate_routing_number, params: { value: "not a routing number" }
+        body = JSON.parse(response.body)
+        expect(ColumnService).not_to receive(:get)
+        expect(body["valid"]).to be_truthy
+      end
     end
 
-    it "handles unknown routing numbers" do
-      get :validate_routing_number, params: { value: "123456789" }
+    context "when value param is in invalid format" do
+      it "is invalid" do
+        get :validate_routing_number, params: { value: "INVALID_FORMAT" }
 
-      expect(response.body).to match(/"valid":false/)
+        body = JSON.parse(response.body)
+
+        expect(ColumnService).not_to receive(:get)
+        expect(body["valid"]).to be_falsy
+        expect(body["hint"]).to eq "Bank not found for this routing number."
+      end
     end
 
-    it "returns bank name" do
-      get :validate_routing_number, params: { value: "083000137" }
+    context "when using ColumnService to get instructions" do
+      let(:param_value) { "123456789" }
+      context "when routing number type is different than aba" do
+        it "is invalid" do
+          stub_request(:get, "https://api.column.com/institutions/#{param_value}")
+            .to_return_json(status: 200, body: { routing_number_type: "not_aba" })
 
-      expect(response.body).to eq({ valid: true, hint: "Jpmorgan Chase Bank, Na" }.to_json)
+          get :validate_routing_number, params: { value: param_value }
+          body = JSON.parse(response.body)
+
+          expect(body["valid"]).to be_falsy
+          expect(body["hint"]).to eq "Please enter an ABA routing number."
+        end
+      end
+
+      context "when ach_eligible is false and routing_number_type is 'aba'" do
+        it "is invalid" do
+          stub_request(:get, "https://api.column.com/institutions/#{param_value}")
+            .to_return_json(status: 200, body: { ach_eligible: false, routing_number_type: "aba" })
+
+          get :validate_routing_number, params: { value: param_value }
+          body = JSON.parse(response.body)
+
+          expect(body["valid"]).to be_falsy
+          expect(body["hint"]).to eq "This routing number cannot accept ACH transfers."
+        end
+      end
+
+      context "when ach_eligible is true and routing_number_type is 'aba'" do
+        it "is invalid" do
+          stub_request(:get, "https://api.column.com/institutions/#{param_value}")
+            .to_return_json(status: 200, body: { ach_eligible: true, routing_number_type: "aba", full_name: "full name" })
+
+          get :validate_routing_number, params: { value: param_value }
+          body = JSON.parse(response.body)
+
+          expect(body["valid"]).to be_truthy
+          expect(body["hint"]).to eq "Full Name"
+        end
+      end
     end
   end
 end
