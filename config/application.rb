27a62045a73d6c6da0e638c034786c787245dc71
Sam Poder <sampoder@berkeley.edu>
2025-07-28 00:16:17 +0000

    Fix lint

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require_relative "boot"
 
 require "rails/all"
 require_relative "../app/lib/credentials"
-require_relative "../lib/active_storage/previewer/csv_previewer.rb"
+require_relative "../lib/active_storage/previewer/csv_previewer"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
