006708e9950c6f1454008b868ddd161330d155da
David Cornu <davidc@hackclub.com>
2025-08-04 13:26:49 -0400

    Add a `:cash_withdrawal` trait to the `:stripe_authorization` factory

--- spec/factories/stripe_authorization_factory.rb
+++ spec/factories/stripe_authorization_factory.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     merchant_data do
       {
         category: "grocery_stores_supermarkets",
+        category_code: "5411",
         network_id: "1234567890",
         name: "HCB-TEST"
       }
@@ -15,6 +16,17 @@ FactoryBot.define do
       {
         amount: 1000,
       }
+    end
+
+    trait :cash_withdrawal do
+      merchant_data do
+        {
+          category: "automated_cash_disburse",
+          category_code: "6011",
+          network_id: "1234567890",
+          name: "HCB-ATM-TEST"
+        }
+      end
     end
   end
 end
