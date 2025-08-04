fe5341e068e3128b21320853945350ce0d4347aa
Samuel Fernandez <me@sfernandez.dev>
2025-08-04 23:55:52 +0000

    Add email for canceled monthly announcements

--- app/jobs/announcement/seven_day_warning_job.rb
+++ app/jobs/announcement/seven_day_warning_job.rb
@@ -6,7 +6,11 @@ class Announcement
 
     def perform
       Announcement.monthly_for(Date.today).where.not(aasm_state: :published).find_each do |announcement|
-        AnnouncementMailer.with(announcement:).seven_day_warning.deliver_now
+        if announcement.blocks.any?(&:empty?)
+          AnnouncementMailer.with(announcement:).canceled.deliver_now
+        else
+          AnnouncementMailer.with(announcement:).seven_day_warning.deliver_now
+        end
       end
     end
 
