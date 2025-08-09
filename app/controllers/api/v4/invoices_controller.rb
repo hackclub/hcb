d840da527e6ead1d2dfb09b6ea33f80c0e7fdbb0
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-08-09 01:34:28 +0000

    Revert "Mirror frontend behavior for sponsors being updated"
    This reverts commit e928d9ba25b7426e855e11a550707d6206caa202.

--- app/controllers/api/v4/invoices_controller.rb
+++ app/controllers/api/v4/invoices_controller.rb
@@ -16,14 +16,15 @@ module Api
         event = authorize Event.find_by_public_id(params[:organization_id]) || Event.friendly.find(params[:organization_id])
         authorize event, :create?, policy_class: InvoicePolicy
 
-        sponsor_attrs = filtered_params[:sponsor_attributes]
+        filtered_params = params.require(:invoice).permit(
+          :due_date,
+          :item_description,
+          :item_amount
+        )
 
         due_date = filtered_params["due_date"].to_datetime
 
-        # we use public ids most places in the api so we need to convert this to a model to get the regular id
-        # if this record doesnt exist it will fall back to the sponsor attributes provided and generate a sponsor
-        # if the record does exist it will be updated by the invoice service to the new sponsor attributes provided here
-        sponsor = authorize Sponsor.find_by_public_id(filtered_params[:sponsor_id])
+        sponsor = authorize Sponsor.find_by_public_id(params[:sponsor_id])
 
         @invoice = ::InvoiceService::Create.new(
           event_id: event.id,
@@ -32,14 +33,14 @@ module Api
           item_amount: filtered_params[:item_amount],
           current_user:,
           sponsor_id: sponsor.id,
-          sponsor_name: sponsor_attributes[:name],
-          sponsor_email: sponsor_attributes[:contact_email],
-          sponsor_address_line1: sponsor_attributes[:address_line1],
-          sponsor_address_line2: sponsor_attributes[:address_line2],
-          sponsor_address_city: sponsor_attributes[:address_city],
-          sponsor_address_state: sponsor_attributes[:address_state],
-          sponsor_address_postal_code: sponsor_attributes[:address_postal_code],
-          sponsor_address_country: sponsor_attributes[:address_country]
+          sponsor_name: sponsor.name,
+          sponsor_email: sponsor.contact_email,
+          sponsor_address_line1: sponsor.address_line1,
+          sponsor_address_line2: sponsor.address_line2,
+          sponsor_address_city: sponsor.address_city,
+          sponsor_address_state: sponsor.address_state,
+          sponsor_address_postal_code: sponsor.address_postal_code,
+          sponsor_address_country: sponsor.address_country
         ).run
 
         render :show
@@ -51,19 +52,6 @@ module Api
         return render json: { error: e.message }, status: :internal_server_error
       end
 
-      private
-
-      def filtered_params
-        params.require(:invoice).permit(
-          :due_date,
-          :item_description,
-          :item_amount,
-          :sponsor_id,
-          sponsor_attributes: policy(Sponsor).permitted_attributes
-        )
-      end
-
     end
-
   end
 end
