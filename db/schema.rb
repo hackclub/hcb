40f737d1e145b4366ef9c03d04bde616c57a2c60
David Cornu <davidc@hackclub.com>
2025-08-11 10:11:06 -0400

    Remove extra space

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_08_08_033500) do
-
   create_schema "google_sheets"
 
   # These are extensions that must be enabled in order to support this database
