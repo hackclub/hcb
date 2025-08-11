c1e7153ac4449a38d8d01c5c7b8b3a257c1d6101
Samuel Fernandez <me@sfernandez.dev>
2025-08-11 20:31:52 +0000

    Remove queue.shift

--- app/jobs/monthly_donation_summary_job.rb
+++ app/jobs/monthly_donation_summary_job.rb
@@ -4,7 +4,6 @@ class MonthlyDonationSummaryJob < ApplicationJob
   queue_as :default
   def perform
     Event.includes(:donations).where("donations.created_at > ?", 1.month.ago).references(:donations).find_each do |event|
-      queue.shift
       EventMailer.with(event: event).monthly_donation_summary.deliver_later
     end
   end
