c1f8c0ffd42c40ca2711df06f8e1dcb4a5702a1c
Samuel Fernandez <me@sfernandez.dev>
2025-08-11 21:01:20 +0000

    Delete monthly announcement drafts when generate is disabled

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -360,6 +360,9 @@ class EventsController < ApplicationController
           @event.plan.mark_inactive!(plan_param) # deactivate old plan and replace it
         end
 
+        if @event.config.generate_monthly_announcement_previously_changed? && !@event.config.generate_monthly_announcement
+          @event.announcements.monthly_for(Date.today).first&.destroy!
+        end
 
         if @event.description_previously_changed? && @event.description.present?
           announcement = Announcement::Templates::NewMissionStatement.new(
