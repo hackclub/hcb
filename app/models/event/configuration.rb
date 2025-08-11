3d040aff782b548f8fdd1791790c9c257cc63b3f
Samuel Fernandez <me@sfernandez.dev>
2025-08-11 21:18:30 +0000

    Add check to make sure two monthly announcements don't get generated for the same event

--- app/models/event/configuration.rb
+++ app/models/event/configuration.rb
@@ -41,7 +41,7 @@ class Event
     def create_or_destroy_monthly_announcement
       if self.generate_monthly_announcement_previously_changed?
         if self.generate_monthly_announcement
-          Announcement::Templates::Monthly.new(event: self.event, author: User.system_user).create
+          Announcement::Templates::Monthly.new(event: self.event, author: User.system_user).create if self.event.announcements.monthly_for(Date.today).empty?
         else
           self.event.announcements.monthly_for(Date.today).first&.destroy!
         end
