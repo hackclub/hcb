9c08c1f3f8f3905b5b8797aad7207b0bea120488
Albert Chae <albertchae@users.noreply.github.com>
2025-08-12 03:13:35 -0700

    Use ActiveSupport::ExecutionContext for controller#action or job info

--- config/initializers/deprecation_warnings.rb
+++ config/initializers/deprecation_warnings.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-ActiveSupport::Notifications.subscribe('deprecation.rails') do |_name, _start, _finish, _id, payload|
+ActiveSupport::Notifications.subscribe("deprecation.rails") do |_name, _start, _finish, _id, payload|
   message = payload[:message]
   callstack = payload[:callstack]
   deprecation_horizon = payload[:deprecation_horizon]
-  gem_name = payload[:gem_name] || 'rails'
+  gem_name = payload[:gem_name] || "rails"
 
-  # there doesn't seem to be a great way to get controller#action here without some more ceremony
-  # so hopefully just the controller and stacktrace will be enough for us to narrow down any issues.
-  class_name = "#{Thread.current[:public_activity_controller]&.class}"
+  execution_context = ActiveSupport::ExecutionContext.to_h
+  appsignal_action = (execution_context[:controller] || execution_context[:job]).class.to_s
+  if execution_context[:controller]
+    appsignal_action += "##{execution_context[:controller].action_name}"
+  end
 
   Appsignal.report_error(ActiveSupport::DeprecationException.new(message)) do
     Appsignal.set_namespace("deprecation.rails")
-    Appsignal.set_action(class_name)
+    Appsignal.set_action(appsignal_action)
     Appsignal.add_tags(
       deprecation: true,
       gem: gem_name,
