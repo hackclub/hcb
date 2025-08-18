5bd269b6d9c9ce844f870dfcfb15ade1c7de82b3
Sam Poder <39828164+sampoder@users.noreply.github.com>
2025-08-18 07:38:01 -0500

    Update recurring_job.rb

--- app/jobs/user/update_card_locking/recurring_job.rb
+++ app/jobs/user/update_card_locking/recurring_job.rb
@@ -5,7 +5,7 @@ class User
     class RecurringJob < ApplicationJob
       queue_as :low
       def perform
-        User.where(cards_locked: true).each do |user|
+        User.where(cards_locked: true).find_each(batch_size: 100) do |user|
           ::UserService::UpdateCardLocking.new(user:).run
         end
       end
