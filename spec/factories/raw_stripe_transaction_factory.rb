dccd4fce0c94abee01ef8432233873ceadf100dd
David Cornu <davidc@hackclub.com>
2025-08-20 13:20:41 -0400

    Add `RawStripeTransaction#merchant_category_key`

--- spec/factories/raw_stripe_transaction_factory.rb
+++ spec/factories/raw_stripe_transaction_factory.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :raw_stripe_transaction do
     amount_cents { Faker::Number.number(digits: 4) }
     unique_bank_identifier { "STRIPEISSUING1" }
-    stripe_transaction { { "merchant_data": { "name": Faker::Name.name } } }
+    stripe_transaction { { "merchant_data": { "name": Faker::Name.name, "category": "bakeries" } } }
   end
 end
