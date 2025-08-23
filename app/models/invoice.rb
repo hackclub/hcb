46b1e1719c2eb5bf95793e50e381b9e37a54264e
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-08-23 17:33:40 -0400

    Update app/models/invoice.rb
    Co-authored-by: David Cornu <me@davidcornu.com>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -187,7 +187,7 @@ class Invoice < ApplicationRecord
   before_create :set_defaults
 
   after_create_commit -> {
-    unless OrganizerPosition.find_by(user: self.creator, event: event)&.manager?
+    unless OrganizerPosition.find_by(user: creator, event: event)&.manager?
       InvoiceMailer.with(invoice: self).notify_organizers_sent.deliver_later
     end
   }
