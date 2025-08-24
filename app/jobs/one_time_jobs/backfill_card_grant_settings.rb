a7709003c2b88c03a4320b44f1b64a947b1e87d6
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-08-24 00:44:44 +0000

    Update job to reflect data better

--- app/jobs/one_time_jobs/backfill_card_grant_settings.rb
+++ app/jobs/one_time_jobs/backfill_card_grant_settings.rb
@@ -4,20 +4,22 @@ module OneTimeJobs
   class BackfillCardGrantSettings
     def self.perform
       CardGrant.find_each do |cg|
-        default_card_grant_setting = CardGrantSetting.find_by(event_id: cg.id)
+        default_cg_setting = CardGrantSetting.find_by(event_id: cg.id)
         ActiveRecord::Base.transaction do
-          card_grant_setting = CardGrantSetting.find_or_create_by!(card_grant_id: cg.id)
-          card_grant_setting.update!({
-                                       banned_categories: cg.banned_categories || default_card_grant_setting.banned_categories,
-                                       banned_merchants: cg.banned_merchants || default_card_grant_setting.banned_merchants,
-                                       category_lock: cg.category_lock || default_card_grant_setting.category_lock,
-                                       expiration_preference: default_card_grant_setting.expiration_preference,
-                                       invite_message: default_card_grant_setting.invite_message,
-                                       keyword_lock: cg.keyword_lock || default_card_grant_setting.keyword_lock,
-                                       merchant_lock: cg.merchant_lock || default_card_grant_setting.merchant_lock,
-                                       pre_authorization_required: cg.pre_authorization_required || default_card_grant_setting.pre_authorization_required,
-                                       reimbursement_conversions_enabled: default_card_grant_setting.reimbursement_conversions_enabled
-                                     })
+          cg_setting = CardGrantSetting.find_or_create_by!(card_grant_id: cg.id)
+          cg_setting.update!(
+            {
+              banned_categories: (cg.banned_categories + (default_cg_setting&.banned_categories || [])).uniq,
+              banned_merchants: (cg.banned_merchants || (default_cg_setting&.banned_merchants || [])).uniq,
+              category_lock: (cg.category_lock || (default_cg_setting&.category_lock || [])).uniq,
+              expiration_preference: default_cg_setting.expiration_preference,
+              invite_message: default_cg_setting.invite_message,
+              keyword_lock: cg.keyword_lock || default_cg_setting&.keyword_lock,
+              merchant_lock: (cg.merchant_lock || (default_cg_setting&.merchant_lock || [])).uniq,
+              pre_authorization_required: [cg.pre_authorization_required, default_cg_setting.pre_authorization_required].first(&:present?),
+              reimbursement_conversions_enabled: default_cg_setting.reimbursement_conversions_enabled
+            }
+          )
         end
       end
     end
