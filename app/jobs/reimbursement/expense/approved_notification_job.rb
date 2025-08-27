5ef4d048dca253cd30e8a04841f29ac6f7148dbf
Samuel Fernandez <me@sfernandez.dev>
2025-08-27 21:23:23 +0000

    Change job to use filter_map, uniq, and group_by

--- app/jobs/reimbursement/expense/approved_notification_job.rb
+++ app/jobs/reimbursement/expense/approved_notification_job.rb
@@ -9,20 +9,14 @@ module Reimbursement
         approvals = PaperTrail::Version.where_object_changes_to(aasm_state: "approved")
                                        .select { |version| version.item_type == "Reimbursement::Expense" && version.created_at > 20.minutes.ago }
 
-        approved_expenses_by_report = {}
-
-        approvals.each do |approval|
+        approved_expenses_without_report = approvals.filter_map do |approval|
           expense = Reimbursement::Expense.find(approval.item_id)
           report = expense.report
 
-          next unless expense.approved? && report.submitted?
+          expense if expense.approved? && report.submitted?
+        end.uniq
 
-          unless approved_expenses_by_report.key?(report)
-            approved_expenses_by_report[report] = []
-          end
-
-          approved_expenses_by_report[report].append(expense)
-        end
+        approved_expenses_by_report = approved_expenses_without_report.group_by { |expense| expense.report }
 
         approved_expenses_by_report.each do |report, expenses|
           ReimbursementMailer.with(report:, expenses:).expenses_approved.deliver_later
