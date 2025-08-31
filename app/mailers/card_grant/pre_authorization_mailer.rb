ceccfef4dd545684f7e33ee54afe217fb323ff35
Samuel Fernandez <me@sfernandez.dev>
2025-08-31 20:20:06 +0000

    Add event name to email

--- app/mailers/card_grant/pre_authorization_mailer.rb
+++ app/mailers/card_grant/pre_authorization_mailer.rb
@@ -5,7 +5,7 @@ class CardGrant
     def notify_fraudulent
       @pre_authorization = params[:pre_authorization]
 
-      mail to: @pre_authorization.event.organizer_contact_emails(only_managers: true), subject: "A card grant pre-authorization requires your review"
+      mail to: @pre_authorization.event.organizer_contact_emails(only_managers: true), subject: "[#{@pre_authorization.event.name}] A card grant pre-authorization requires your review"
     end
 
   end
