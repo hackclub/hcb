b927ee01251e77b8b53c531f617d0fd2c04e25e8
Samuel Fernandez <me@sfernandez.dev>
2025-09-22 23:00:18 +0000

    Add backfill job

---
+++ app/jobs/one_time_jobs/backfill_owners.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module OneTimeJobs
+  class BackfillOwners
+    def self.perform
+      OrganizerPosition.find_each do |op|
+        if op.is_signee
+          op.update!(role: :owner)
+        end
+      end
+    end
+
+  end
+end
