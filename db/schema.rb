d914db159cc9b31f8a56a99547ccbf433fe7c12e
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-09-23 19:57:00 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_05_005653) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_23_195446) do
   create_schema "google_sheets"
 
   # These are extensions that must be enabled in order to support this database
@@ -1015,6 +1015,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_05_005653) do
     t.boolean "financially_frozen", default: false, null: false
     t.boolean "donation_tiers_enabled", default: false, null: false
     t.bigint "parent_id"
+    t.boolean "fee_waiver_eligible", default: false, null: false
     t.index ["parent_id"], name: "index_events_on_parent_id"
     t.index ["point_of_contact_id"], name: "index_events_on_point_of_contact_id"
   end
