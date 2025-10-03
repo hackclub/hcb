1733003253cac3c747ca83ae7b0344e2d1e22a36
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-10-03 01:54:01 +0100

    Add command handler and command registration

--- app/controllers/discord_controller.rb
+++ app/controllers/discord_controller.rb
@@ -4,25 +4,12 @@ require "ed25519"
 require "discordrb"
 
 class DiscordController < ApplicationController
-  protect_from_forgery except: :webhook
-  skip_before_action :signed_in_user, only: :webhook
+  protect_from_forgery except: [:webhook, :interaction]
+  skip_before_action :signed_in_user, only: [:webhook, :interaction]
+  before_action :verify_discord_signature, only: [:webhook, :interaction]
   skip_after_action :verify_authorized
 
   def webhook
-    timestamp = request.headers["X-Signature-Timestamp"]
-    signature_hex = request.headers["X-Signature-Ed25519"]
-    signature = [signature_hex].pack("H*")
-    key = [ENV["DISCORD_PUBLIC_KEY"]].pack("H*")
-
-    verify_key = Ed25519::VerifyKey.new(key)
-
-    begin
-      verify_key.verify(signature, timestamp + request.raw_post)
-    rescue Ed25519::VerifyError
-      head :unauthorized
-      return
-    end
-
     # Webhook event where the bot was added to a server
     if (params[:type] = 1) && (params[:event][:type] = "APPLICATION_AUTHORIZED") && params[:event][:data][:integration_type] = 0
       user_id = params[:event][:data][:user][:id]
@@ -32,6 +19,12 @@ class DiscordController < ApplicationController
     end
 
     head :no_content
+  end
+
+  def interaction
+    puts "ACKing"
+
+    render json: { type: 1 } # ACK the interaction
   end
 
   def link
@@ -50,7 +43,23 @@ class DiscordController < ApplicationController
   private
 
   def bot
-    @bot ||= Discordrb::Bot.new token: ENV["DISCORD_BOT_TOKEN"]
+    @bot ||= Discordrb::Bot.new token: Credentials.fetch(:DISCORD__BOT_TOKEN)
+  end
+
+  def verify_discord_signature
+    timestamp = request.headers["X-Signature-Timestamp"]
+    signature_hex = request.headers["X-Signature-Ed25519"]
+    signature = [signature_hex].pack("H*")
+    key = [Credentials.fetch(:DISCORD__PUBLIC_KEY)].pack("H*")
+
+    verify_key = Ed25519::VerifyKey.new(key)
+
+    begin
+      verify_key.verify(signature, timestamp + request.raw_post)
+    rescue Ed25519::VerifyError
+      head :unauthorized
+      return
+    end
   end
 
 end
