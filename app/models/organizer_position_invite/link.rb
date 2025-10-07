fcb728914768da5cc40425678293bac6af60c1c8
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-10-07 19:56:30 -0400

    Update app/models/organizer_position_invite/link.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/models/organizer_position_invite/link.rb
+++ app/models/organizer_position_invite/link.rb
@@ -33,7 +33,7 @@ class OrganizerPositionInvite
     belongs_to :creator, class_name: "User"
     belongs_to :deactivator, class_name: "User"
 
-    has_many :requests, class_name: "OrganizerPositionInvite::Request", foreign_key: :link_id, dependent: :destroy
+    has_many :requests, class_name: "OrganizerPositionInvite::Request", dependent: :destroy
 
     scope :active, -> { where(deactivated_at: nil) }
     scope :usable, -> { active.where("? <= created_at + expires_in", Time.now) }
