7088d493643a0464a77f442752cc3939e07fa58f
Gary Tou <gary@garytou.com>
2025-10-07 16:18:19 -0700

    Fix typo (`validate` -> `validates`)

--- app/models/user.rb
+++ app/models/user.rb
@@ -185,7 +185,7 @@ class User < ApplicationRecord
 
   validate(:admins_cannot_disable_2fa, on: :update)
 
-  validate :discord_id, uniqueness: { message: "is already linked to another user. Please contact hcb@hackclub.com if this is unexpected." }
+  validates :discord_id, uniqueness: { message: "is already linked to another user. Please contact hcb@hackclub.com if this is unexpected." }
 
   enum :comment_notifications, { all_threads: 0, my_threads: 1, no_threads: 2 }
 
