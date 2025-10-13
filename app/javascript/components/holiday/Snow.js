ea7003e0f5df157ed1ecdd908bcd60207735cfd8
yodalightsabr <yoda@yodacode.xyz>
2025-10-13 16:00:10 -0700

    Update app/javascript/components/holiday/Snow.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/javascript/components/holiday/Snow.js
+++ app/javascript/components/holiday/Snow.js
@@ -10,7 +10,17 @@ export default function Snow() {
   return (
     <>
       {snow ? (
-        <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 9999, pointerEvents: 'none' }}>
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 9999,
+            pointerEvents: 'none',
+          }}
+        >
           <Snowfall />
         </div>
       ) : null}
