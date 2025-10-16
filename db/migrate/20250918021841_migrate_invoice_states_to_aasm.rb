9f5721abb7c7786876ccd4a9dd15363b330dc91e
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-10-16 17:45:27 -0400

    actually push changes

--- db/migrate/20250918021841_migrate_invoice_states_to_aasm.rb
+++ db/migrate/20250918021841_migrate_invoice_states_to_aasm.rb
@@ -1,4 +1,17 @@
 class MigrateInvoiceStatesToAasm < ActiveRecord::Migration[7.2]
-  def change
+  disable_ddl_transaction!
+
+  def up
+    Invoice.find_in_batches(batch_size: 100) do |batch|
+      batch.each do |invoice|
+        if invoice.deposited?
+          invoice.update_column(:aasm_state, :deposited_v2)
+        end
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
