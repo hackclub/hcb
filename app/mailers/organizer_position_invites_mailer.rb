e1893d2e8bbcb47d53ccb7a390adcad631efea77
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-10-17 14:12:10 +0000

    Don't send OPI emails when there is an OPIR

--- app/mailers/organizer_position_invites_mailer.rb
+++ app/mailers/organizer_position_invites_mailer.rb
@@ -4,18 +4,24 @@ class OrganizerPositionInvitesMailer < ApplicationMailer
   before_action :set_invite
 
   def notify
-    mail to: @invite.user.email_address_with_name, subject: @invite.initial? && @invite.event.demo_mode? ? "Thanks for applying for HCB 🚀" : "You've been invited to join #{@invite.event.name} on HCB 🚀"
+    # if somebody gets invited with a link, removed, and then invited regularly they wont get an email here
+    # we dont currently assosciate OPI and OPIR / OPIL
+    unless @invite.event.organizer_position_invite_requests.approved.where(requester: current_user).any?
+      mail to: @invite.user.email_address_with_name, subject: @invite.initial? && @invite.event.demo_mode? ? "Thanks for applying for HCB 🚀" : "You've been invited to join #{@invite.event.name} on HCB 🚀"
+    end
   end
 
   def accepted
-    @emails = (@invite.event.users.excluding(@invite.user).map(&:email_address_with_name) + [@invite.event.config.contact_email]).compact
-
-    @announcement = Announcement::Templates::NewTeamMember.new(
-      invite: @invite,
-      author: User.system_user
-    ).create
-
-    mail to: @emails, subject: "#{@invite.user.name} has accepted their invitation to join #{@invite.event.name}"
+    unless @invite.event.organizer_position_invite_requests.approved.where(requester: current_user).any?
+      @emails = (@invite.event.users.excluding(@invite.user).map(&:email_address_with_name) + [@invite.event.config.contact_email]).compact
+      
+      @announcement = Announcement::Templates::NewTeamMember.new(
+        invite: @invite,
+        author: User.system_user
+      ).create
+      
+      mail to: @emails, subject: "#{@invite.user.name} has accepted their invitation to join #{@invite.event.name}"
+    end
   end
 
   private
