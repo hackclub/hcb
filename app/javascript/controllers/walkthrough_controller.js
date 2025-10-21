e22cc60f13c64ad392d7930fb36fcd720b1db62b
Manu Gurudath <manusvathgurudath@gmail.com>
2025-10-21 20:53:54 +0000

    Add walkthrough controller

---
+++ app/javascript/controllers/walkthrough_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = []
+  static values = {
+    key: String,
+    progress: Number,
+  }
+
+  connect() {
+
+  }
+
+  save() {
+
+  }
+}
