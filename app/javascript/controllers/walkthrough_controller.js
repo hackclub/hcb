e6c60ef7087e79463b5ce2bcb5d56d011db49542
Manu Gurudath <manusvathgurudath@gmail.com>
2025-10-22 18:19:11 +0000

    Add walkthrough content for donations and update UI prompts

--- app/javascript/controllers/walkthrough_controller.js
+++ app/javascript/controllers/walkthrough_controller.js
@@ -7,6 +7,26 @@ export default class extends Controller {
     progress: Number,
   }
 
+  static WALKTHROUGHS = {
+    donations: [
+      {
+        element: null,
+        title: "Welcome to Donations",
+        content: "HCB's donations feature provides a customizable donation page that can be shared online. Donors can choose one-time or recurring donations, and you can customize tiers & design.",
+      },
+      {
+        element: null,
+        title: "Share",
+        content: "Customize donation page links, embed your form, generate QR codes, and share real-time donor graphs"
+      },
+      {
+        element: null,
+        title: "Customize your donation page",
+        content: "You can display banners, create donation tiers, and set donation goals here"
+      },
+    ]
+  }
+
   connect() {
 
   }
