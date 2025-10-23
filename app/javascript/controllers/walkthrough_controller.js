77132d13ada0681d852d7eb1ce9a1b54fde3ce16
Manu Gurudath <manusvathgurudath@gmail.com>
2025-10-23 01:56:34 +0000

    Add reimbursements to walkthrough progress

--- app/javascript/controllers/walkthrough_controller.js
+++ app/javascript/controllers/walkthrough_controller.js
@@ -11,27 +11,40 @@ export default class extends Controller {
     donations: [
       {
         element: null,
-        title: "Welcome to Donations",
-        content: "HCB's donations feature provides a customizable donation page that can be shared online. Donors can choose one-time or recurring donations, and you can customize tiers & design.",
+        title: 'Welcome to Donations',
+        content:
+          "HCB's donations feature provides a customizable donation page that can be shared online. Donors can choose one-time or recurring donations, and you can customize tiers & design.",
       },
       {
         element: null,
-        title: "Share",
-        content: "Customize donation page links, embed your form, generate QR codes, and share real-time donor graphs"
+        title: 'Share',
+        content:
+          'Customize donation page links, embed your form, generate QR codes, and share real-time donor graphs',
       },
       {
         element: null,
-        title: "Customize your donation page",
-        content: "You can display banners, create donation tiers, and set donation goals here"
+        title: 'Customize your donation page',
+        content:
+          'You can display banners, create donation tiers, and set donation goals here',
       },
-    ]
+    ],
+    reimbursements: [
+      {
+        element: null,
+        title: 'Welcome to Reimbursements',
+        content:
+          'Reimbursements are when you repay someone with funds from your HCB account. An example of this would be if you forgot your physical HCB card at home so you purchased something for your organization with your personal money, so you would pay yourself back with funds from your HCB account.',
+      },
+      {
+        element: null,
+        title: 'Create a Reimbursement',
+        content:
+          'To create a reimbursement, click the "New Reimbursement" button and fill out the required fields.',
+      },
+    ],
   }
 
-  connect() {
+  connect() {}
 
-  }
-
-  save() {
-
-  }
+  save() {}
 }
