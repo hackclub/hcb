a94bfe03632b9e95c495d9d2a1b25810529e1e99
Samuel Fernandez <me@sfernandez.dev>
2025-10-27 20:47:23 +0000

    Edit notice mailer to clarify monthly announcements

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -29,7 +29,13 @@ class AnnouncementMailer < ApplicationMailer
     @monthly_announcement = params[:monthly_announcement]
     @scheduled_for = Date.today.next_month.beginning_of_month
 
-    mail to: @emails, subject: "[#{@event.name}] Monthly announcements have been enabled for your organization"
+    @cancellation_email = Ahoy::Message.where(sent_at: Date.today.beginning_of_month.., subject: "[#{@event.name}] Your scheduled monthly announcement has been canceled").first
+
+    if @cancellation_email.present?
+      mail to: @emails, subject: "[#{@event.name}] Explaining monthly announcements"
+    else
+      mail to: @emails, subject: "[#{@event.name}] Monthly announcements are enabled for your organization"
+    end
   end
 
   def set_warning_variables
