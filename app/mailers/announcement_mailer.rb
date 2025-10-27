d9e8ac145fa70ce7df29935c07a0403f3699a0af
Samuel Fernandez <me@sfernandez.dev>
2025-10-27 22:04:29 +0000

    Remove check for warning email

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -31,18 +31,7 @@ class AnnouncementMailer < ApplicationMailer
 
     @cancellation_email = Ahoy::Message.where(sent_at: Date.today.beginning_of_month.., subject: "[#{@event.name}] Your scheduled monthly announcement has been canceled").first
 
-    subject = "[#{@event.name}] Explaining monthly announcements"
-
-    unless @cancellation_email.present?
-      scheduled_for = Date.today.next_month.beginning_of_month
-      @warning_email = Ahoy::Message.where(sent_at: Date.today.beginning_of_month.., subject: "[#{@event.name}] Your scheduled monthly announcement will be delivered on #{scheduled_for.strftime("%B #{scheduled_for.day.ordinalize}")}").first
-
-      if @warning_email.nil?
-        subject = "[#{@event.name}] Monthly announcements are enabled for your organization"
-      end
-    end
-
-    mail to: @emails, subject:
+    mail to: @emails, subject: "[#{@event.name}] #{@cancellation_email.present? ? "Explaining monthly announcements" : "Monthly announcements are enabled for your organization"}"
   end
 
   def set_warning_variables
