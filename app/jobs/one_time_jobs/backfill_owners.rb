88850f786d94723ac18f539feb270fe3c93e6e58
Samuel Fernandez <me@sfernandez.dev>
2025-11-07 02:21:41 +0000

    Fix validation errors in backfill job

--- app/jobs/one_time_jobs/backfill_owners.rb
+++ app/jobs/one_time_jobs/backfill_owners.rb
@@ -9,9 +9,9 @@ module OneTimeJobs
         end
       end
 
-      OrganizerPositionInvite.find_each do |op|
-        if op.is_signee
-          op.update!(role: :owner)
+      OrganizerPositionInvite.find_each do |opi|
+        if opi.is_signee && !opi.event&.users&.pluck(:email)&.include?(opi.user.email)
+          opi.update!(role: :owner)
         end
       end
     end
