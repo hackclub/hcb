fbe1c667e09cf1728b5e9857b27298fce4cee0e7
Manu Gurudath <manusvathgurudath@gmail.com>
2025-11-10 16:44:11 +0000

    Fix capitalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include RedirectToUI3
+  include RedirectToUi3
 
   include Pundit::Authorization
   include SessionsHelper
--- app/controllers/concerns/redirect_to_ui3.rb
+++ app/controllers/concerns/redirect_to_ui3.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module RedirectToUI3
+module RedirectToUi3
   extend ActiveSupport::Concern
 
   included do
@@ -11,7 +11,6 @@ module RedirectToUI3
 
   def redirect_to_ui3_if_needed
     return if current_user.nil?
-    # Only redirect GET and HEAD requests - POST/PUT/DELETE should reach the backend
     return unless request.get? || request.head?
 
     if Flipper.enabled?(:redirect_to_ui3_2025_11_03, current_user) && request.host == "hcb.hackclub.com"
