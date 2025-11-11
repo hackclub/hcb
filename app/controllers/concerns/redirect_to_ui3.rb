99700beac50a185dc7e41a98f2f4f274a7f56e6d
Manu Gurudath <manusvathgurudath@gmail.com>
2025-11-11 00:35:28 +0000

    fix lint

--- app/controllers/concerns/redirect_to_ui3.rb
+++ app/controllers/concerns/redirect_to_ui3.rb
@@ -11,6 +11,7 @@ module RedirectToUi3
 
   def redirect_to_ui3_if_needed
     return if current_user.nil?
+
     # return unless request.get? || request.head?
 
     if Flipper.enabled?(:redirect_to_ui3_2025_11_03, current_user) && request.host == "hcb.hackclub.com"
