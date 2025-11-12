02b4872b27f5cec26cc0516f690a044601adb1f4
Samuel Fernandez <me@sfernandez.dev>
2025-11-12 01:03:45 +0000

    Copy over records in db migration

--- db/migrate/20251111025217_create_contracts.rb
+++ db/migrate/20251111025217_create_contracts.rb
@@ -1,5 +1,5 @@
 class CreateContracts < ActiveRecord::Migration[8.0]
-  def change
+  def up
     create_table :contracts do |t|
       t.string :aasm_state
       t.string :cosigner_email
@@ -17,5 +17,35 @@ class CreateContracts < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
+
+    # migrate records from organizer_position_contracts
+    OrganizerPosition::Contract.all.in_batches(of: 1000).each do |relation|
+      new_rows = relation.map do |row|
+        {
+          id: row.id,
+          aasm_state: row.aasm_state,
+          cosigner_email: row.cosigner_email,
+          external_service: row.external_service,
+          include_videos: row.include_videos,
+          external_id: row.external_id,
+          signed_at: row.signed_at,
+          void_at: row.void_at,
+          deleted_at: row.deleted_at,
+          contractable: row.organizer_position_invite,
+          document: row.document,
+          organizer_position: row.organizer_position
+        }
+
+      ActiveRecord::Base.transaction { Contract.insert_all(new_rows) }
+      end
+    end
+    
+    # rename papertrail item types
+    PaperTrail::Version.where(item_type: "OrganizerPosition::Contract").update_all(item_type: "Contract")
+  end
+
+  def down
+    drop_table :contracts
+    PaperTrail::Version.where(item_type: "Contract").update_all(item_type: "OrganizerPosition::Contract")
   end
 end
