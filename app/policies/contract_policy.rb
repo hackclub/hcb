987421ef12c73ceaf4be9b06522d764a1a97f07e
Samuel Fernandez <me@sfernandez.dev>
2025-11-12 02:50:42 +0000

    Rename ContractPolicy

--- app/policies/contract_policy.rb
+++ app/policies/contract_policy.rb
@@ -1,23 +1,20 @@
 # frozen_string_literal: true
 
-class OrganizerPosition
-  class ContractPolicy < ApplicationPolicy
-    def create?
-      user&.admin?
-    end
+class ContractPolicy < ApplicationPolicy
+  def create?
+    user&.admin?
+  end
 
-    def void?
-      user&.admin?
-    end
+  def void?
+    user&.admin?
+  end
 
-    def resend_to_user?
-      user&.admin?
-    end
+  def resend_to_user?
+    user&.admin?
+  end
 
-    def resend_to_cosigner?
-      user&.admin?
-    end
-
+  def resend_to_cosigner?
+    user&.admin?
   end
 
 end
