b06b40c15e91ee5f641c3f1e3831d277ace9344a
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-11-13 14:22:23 -0800

    Change BackfillDepositedStateForInvoices to inherit from ApplicationJob

--- app/jobs/one_time_jobs/backfill_deposited_state_for_invoices.rb
+++ app/jobs/one_time_jobs/backfill_deposited_state_for_invoices.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module OneTimeJobs
-  class BackfillDepositedStateForInvoices
+  class BackfillDepositedStateForInvoices < ApplicationJob
     queue_as :default
 
     def perform
