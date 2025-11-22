c51449951a3bda30eb03172d02506d89d06ecfa9
sampoder <39828164+sampoder@users.noreply.github.com>
2025-11-22 11:50:10 -0800

    Pass in `event` to `receipt_optional?` optionally

--- app/models/hcb_code.rb
+++ app/models/hcb_code.rb
@@ -497,7 +497,10 @@ class HcbCode < ApplicationRecord
               ")
   }
 
-  def receipt_required?
+  # we optionally take an event parameter here. this
+  # is a performance optimisation because it allows us to
+  # load the event once on the ledger and never again
+  def receipt_required?(event = event)
     return false if pt&.declined?
 
     return false unless event&.plan&.receipts_required?
@@ -511,8 +514,8 @@ class HcbCode < ApplicationRecord
     false
   end
 
-  def receipt_optional?
-    !receipt_required?
+  def receipt_optional?(event = event)
+    !receipt_required?(event)
   end
 
   def receipts
