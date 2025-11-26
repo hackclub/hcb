4c96ee3e108c9fb8b62dad0e5388abe2d1b37428
Manu Gurudath <manusvathgurudath@gmail.com>
2025-11-26 19:29:22 +0000

    fix: add authorization and redirect for event edit tab

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -323,9 +323,12 @@ class EventsController < ApplicationController
 
   # GET /events/1/edit
   def edit
+    authorize @event
+    if !params[:tab] || !%w[details donations reimbursements card_grants tags affiliations features integrations audit_log admin].include?(params[:tab])
+      return redirect_to edit_event_path(@event.slug, tab: "details")
+    end
     @settings_tab = params[:tab]
     @frame = params[:frame]
-    authorize @event
     @activities_before = params[:activities_before] || Time.now
     @activities = PublicActivity::Activity.for_event(@event).before(@activities_before).order(created_at: :desc).page(params[:page]).per(25) if @settings_tab == "audit_log"
     @affiliations = @event.affiliations if @settings_tab == "affiliations"
