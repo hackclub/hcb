415f6dcb915678ef200f2b2b987982638f6db4ca
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-12-02 23:35:35 +0000

    Add one time job

---
+++ app/jobs/one_time_jobs/backfill_grant_expiration.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module OneTimeJobs
+  class BackfillGrantExpiration
+    def self.perform
+      CardGrant.find_each do |cg|
+        cg.update!(expiration_date: cg.created_at + cg.event.card_grant_setting.expiration_preference_before_type_cast.days)
+      end
+    end
+
+  end
+end
