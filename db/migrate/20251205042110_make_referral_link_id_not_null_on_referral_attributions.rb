b736aa3cf4b8a7068ad1b371c12208c138fb99d6
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-12-05 05:47:29 +0000

    Revert "Add nonnull"
    This reverts commit 4edc8e518440e7cf10f3b9bac53bcb57db2141b0.

--- db/migrate/20251205042110_make_referral_link_id_not_null_on_referral_attributions.rb
+++
@@ -1,5 +0,0 @@
-class MakeReferralLinkIdNotNullOnReferralAttributions < ActiveRecord::Migration[8.0]
-  def change
-    add_check_constraint :referral_attributions, "referral_link_id IS NOT NULL", name: "referral_attributions_referral_link_id_null", validate: false
-  end
-end
