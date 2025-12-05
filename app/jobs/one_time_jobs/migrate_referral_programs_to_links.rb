e29c5d37b4b39f6a1f4b44b3a2f7b07c5a088155
yodalightsabr <yoda@yodacode.xyz>
2025-12-05 04:04:52 +0000

    Also update referral attributions

--- app/jobs/one_time_jobs/migrate_referral_programs_to_links.rb
+++ app/jobs/one_time_jobs/migrate_referral_programs_to_links.rb
@@ -4,12 +4,14 @@ module OneTimeJobs
   class MigrateReferralProgramsToLinks < ApplicationJob
     def perform
       Referral::Program.find_each do |program|
-        Referral::Link.create!(
+        link = Referral::Link.create!(
           program:,
           slug: program.hashid,
           creator: program.creator,
           name: "Default link (backfilled from program)"
         )
+
+        program.attributions.update_all(referral_link_id: link.id)
       end
     end
 
