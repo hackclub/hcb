2e44cc4e5594fd48446a87a96a0728fac844206c
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-12-05 05:32:39 +0000

    fix if dont exist

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -29,8 +29,12 @@ class LoginsController < ApplicationController
   def create
     user = User.create_with(creation_method: :login).find_or_create_by!(email: params[:email])
 
-    referral_link = Referral::Link.find_by(slug: params[:referral_link_id]).presence || Referral::Link.find_by_hashid(params[:referral_link_id]) if params[:referral_link_id].present?
-    login = user.logins.create(referral_program: referral_link.program, referral_link:)
+    if params[:referral_link_id].present?
+      referral_link = Referral::Link.find_by(slug: params[:referral_link_id]).presence || Referral::Link.find_by_hashid(params[:referral_link_id])
+      login = user.logins.create(referral_program: referral_link&.program, referral_link:)
+    else
+      login = user.logins.create
+    end
 
     cookies.signed["browser_token_#{login.hashid}"] = { value: login.browser_token, expires: Login::EXPIRATION.from_now }
 
