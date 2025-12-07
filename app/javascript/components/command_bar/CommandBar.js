762712c95ada743933e2ff95c1901fd86af9a1ff
Manu Gurudath <manusvathgurudath@gmail.com>
2025-12-07 02:03:56 +0000

    fix command abr

--- app/javascript/components/command_bar/CommandBar.js
+++ app/javascript/components/command_bar/CommandBar.js
@@ -22,7 +22,7 @@ export default function CommandBar({
   adminUrls = {},
 }) {
   return (
-    <div style={{ position: 'relative', zIndex: '1000' }}>
+    <div style={{ position: 'relative', zIndex: '100000000' }}>
       <KBarProvider
         actions={[
           ...initalActions,
@@ -113,21 +113,21 @@ function SearchAndResults() {
         ),
         ...(search != ''
           ? [
-              {
-                id: 'search',
-                name: 'Search',
-                keywords: 'search',
-                icon: <Icon glyph="search" size={16} />,
-                priority: Priority.HIGH,
-              },
-              {
-                id: `results: ${search}`,
-                parent: currentRootActionId,
-                name: `Loading...`,
-                keywords: search,
-                priority: Priority.HIGH,
-              },
-            ]
+            {
+              id: 'search',
+              name: 'Search',
+              keywords: 'search',
+              icon: <Icon glyph="search" size={16} />,
+              priority: Priority.HIGH,
+            },
+            {
+              id: `results: ${search}`,
+              parent: currentRootActionId,
+              name: `Loading...`,
+              keywords: search,
+              priority: Priority.HIGH,
+            },
+          ]
           : []),
       ])
     }
@@ -211,18 +211,16 @@ function SearchAndResults() {
         defaultPlaceholder={'Search for organizations, pages, actions...'}
         placeholder={
           searched && actions.filter(x => x.id == 'result').length > 0
-            ? `Successfully found ${
-                actions.filter(x => x.id == 'result').length
-              } result${
-                actions.filter(x => x.id == 'result').length > 1 ? 's' : ''
-              }.`
+            ? `Successfully found ${actions.filter(x => x.id == 'result').length
+            } result${actions.filter(x => x.id == 'result').length > 1 ? 's' : ''
+            }.`
             : searched && actions.filter(x => x.error).length > 0
               ? searchedFor
               : searched &&
-                  actions.filter(
-                    x =>
-                      x.id == 'search' && x.parent == `results: ${searchedFor}`
-                  ).length > 0
+                actions.filter(
+                  x =>
+                    x.id == 'search' && x.parent == `results: ${searchedFor}`
+                ).length > 0
                 ? 'Found 0 results.'
                 : null
         }
