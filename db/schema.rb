afb56d5d21acc21cf8fbdb8aa7b6004321a2b810
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-12-08 17:49:37 +0000

    Add DB migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_05_035167) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_08_174819) do
   create_schema "google_sheets"
 
   # These are extensions that must be enabled in order to support this database
