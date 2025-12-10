83fbe499265e734bc177029f7018d8880b16ffc6
Manu Gurudath <manusvathgurudath@gmail.com>
2025-12-10 18:24:45 +0000

    Fix tags

--- app/services/breakdown_engine/tags.rb
+++ app/services/breakdown_engine/tags.rb
@@ -31,28 +31,6 @@ module BreakdownEngine
       tags.sort_by! { |tag| tag[:value] }.reverse!
 
       total_amount = tags.sum { |tag| tag[:value] }
-      threshold = total_amount * 0.05
-
-      if threshold > 0
-        # Update tags to apply the threshold condition
-        tags = tags.map do |tag|
-          {
-            name: tag[:name],
-            truncated: tag[:truncated],
-            value: (tag[:value] >= threshold ? tag[:value] : 0)
-          }
-        end
-
-        # Calculate "Other" amount
-        other_amount = total_amount - tags.sum { |tag| tag[:value] }
-        if other_amount > 0
-          tags << {
-            name: "Other",
-            truncated: "Other",
-            value: other_amount
-          }
-        end
-      end
 
       tags
     end
