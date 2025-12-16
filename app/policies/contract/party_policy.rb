29905e26b1a88e6792a971c2e033ac4928cb5caf
Samuel Fernandez <me@sfernandez.dev>
2025-12-16 14:26:59 -0500

    Allow HCB admins to sign for any HCB party
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/policies/contract/party_policy.rb
+++ app/policies/contract/party_policy.rb
@@ -4,6 +4,8 @@ class Contract
   class PartyPolicy < ApplicationPolicy
     def show?
       if record.user.present?
+        return true if record.role == "hcb" && user.admin?
+
         return record.user == user
       end
 
