2f02b51ff2280e787f98298dc4f10f7e6ead5a3e
Manu Gurudath <manusvathgurudath@gmail.com>
2025-12-20 20:59:01 +0000

    improve code based on @Luke-Oldenburg's feedback

--- app/controllers/api/v4/transactions_controller.rb
+++ app/controllers/api/v4/transactions_controller.rb
@@ -51,9 +51,11 @@ module Api
 
       def missing_receipt
         user_hcb_code_ids = current_user.stripe_cards.flat_map { |card| card.local_hcb_codes.pluck(:id) }
-        user_hcb_codes = HcbCode.where(id: user_hcb_code_ids)
+        user_hcb_codes = HcbCode.where(id: user_hcb_code_ids).includes(:events)
+        hcb_codes_missing_ids = user_hcb_codes.select do |hcb_code|
+          hcb_code.events.any? { |event| hcb_code.missing_receipt?(event) }
+        end.map(&:id)
 
-        hcb_codes_missing_ids = user_hcb_codes.missing_receipt.receipt_required.pluck(:id)
         @hcb_codes = HcbCode.where(id: hcb_codes_missing_ids).order(created_at: :desc)
 
         @total_count = @hcb_codes.size
@@ -75,6 +77,19 @@ module Api
         @hcb_code = authorize HcbCode.find_by_public_id(params[:id]), :update?
 
         @suggested_memos = ::HcbCodeService::SuggestedMemos.new(hcb_code: @hcb_code, event: @event).run.first(4)
+      end
+
+      def mark_no_receipt
+        @hcb_code = HcbCode.find_by_public_id!(params[:id])
+        authorize @hcb_code, :mark_no_or_lost?, policy_class: ReceiptablePolicy
+
+        begin
+          @hcb_code.no_or_lost_receipt!
+        rescue
+          return render json: { error: "invalid_operation", messages: "Failed to mark transaction as no/lost receipt" }, status: :unprocessable_entity
+        end
+
+        render json: { message: "Transaction marked as no/lost receipt" }, status: :ok
       end
 
       private
