cdf70cfd30081afab66a16b951175b2ad7f746b4
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-12-23 07:36:05 +0000

    Update notified mailer

--- app/mailers/organizer_position_invites_mailer.rb
+++ app/mailers/organizer_position_invites_mailer.rb
@@ -4,7 +4,13 @@ class OrganizerPositionInvitesMailer < ApplicationMailer
   before_action :set_invite
 
   def notify
-    mail to: @invite.user.email_address_with_name, subject: @invite.initial? && @invite.event.demo_mode? ? "Thanks for applying for HCB 🚀" : "You've been invited to join #{@invite.event.name} on HCB 🚀"
+    if @invite.organizer_position_invite_request.present?
+      mail to: @invite.user.email_address_with_name, subject: "Your request to join #{@invite.event.name} has been approved"
+    elsif @invite.initial? && @invite.event.demo_mode?
+      mail to: @invite.user.email_address_with_name, subject: "Thanks for applying for HCB 🚀"
+    else
+      mail to: @invite.user.email_address_with_name, subject: "You've been invited to join #{@invite.event.name} on HCB 🚀"
+    end
   end
 
   def accepted
