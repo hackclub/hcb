05a19f9aaf0583e5599f11c21d821a89c362581b
Mohamad Mortada <mmortada685@gmail.com>
2025-12-23 18:11:25 -0800

    [v4] Index tags for organizations

---
+++ app/controllers/api/v4/tags_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module V4
+    class TagsController < ApplicationController
+      include SetEvent
+
+      before_action :set_api_event, only: [:index]
+
+      def index
+        authorize @event, :show_in_v4?
+        @tags = @event.tags.order(created_at: :desc)
+      end
+
+    end
+  end
+end
