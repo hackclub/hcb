3a555ec3326e511e8574c669b58887d208008615
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-12-31 03:42:15 +0000

    Add admin nav items

--- app/models/admin/nav.rb
+++ app/models/admin/nav.rb
@@ -169,6 +169,18 @@ module Admin
             path: admin_event_groups_path,
             count: Event::Group.count,
             count_type: :records,
+          ),
+          make_item(
+            name: "Active Teenagers Leaderboard",
+            path: active_teenagers_leaderboard_admin_index_path,
+            count: User.active_teenager.count,
+            count_type: :records,
+          ),
+          make_item(
+            name: "New Teenagers Leaderboard",
+            path: new_teenagers_leaderboard_admin_index_path,
+            count: Referral::Attribution.where(user: User.where(teenager: true)).count,
+            count_type: :records,
           )
         ]
       )
