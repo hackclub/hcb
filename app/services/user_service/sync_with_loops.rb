6bf925a91f4e1db3548cca1db466f96426241436
Sam Poder <sampoder@berkeley.edu>
2026-01-05 09:34:24 -0600

    Update sync_with_loops.rb

--- app/services/user_service/sync_with_loops.rb
+++ app/services/user_service/sync_with_loops.rb
@@ -69,9 +69,7 @@ module UserService
 
         JSON[resp.body][0]
       rescue => e
-        Rails.error.unexpected(
-          "Received exception #{e.full_message} while attempting to get contact details for email #{@user.email} from Loops.",
-        )
+        Rails.error.unexpected "Received exception #{e.full_message} while attempting to get contact details for email #{@user.email} from Loops."
         raise e
       end
     end
