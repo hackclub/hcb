a53c0b44864fb45360f46de5c767adf650547d68
Samuel Fernandez <me@sfernandez.dev>
2026-01-12 16:36:34 -0500

    Remove unnecessary cascade delete on user applications
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,7 +80,7 @@ class User < ApplicationRecord
   }
 
   has_many :logins
-  has_many :applications, class_name: "Event::Application", inverse_of: :user, dependent: :destroy
+  has_many :applications, class_name: "Event::Application", inverse_of: :user
   has_many :login_codes
   has_many :backup_codes, class_name: "User::BackupCode", inverse_of: :user, dependent: :destroy
   has_many :user_sessions, class_name: "User::Session", dependent: :destroy
