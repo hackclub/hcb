116a1a7a02f4566745db4e5e2255b304c1158fe5
Manu Gurudath <manusvathgurudath@gmail.com>
2026-01-21 05:33:09 +0000

    [Check Preview] Enhance scaling logic and adjust layout for improved responsiveness

--- app/javascript/controllers/check_preview_controller.js
+++ app/javascript/controllers/check_preview_controller.js
@@ -15,26 +15,34 @@ export default class extends Controller {
     const tableOfContents = document.getElementById('table-of-contents')
     const contactInformation = document.getElementById('contact-information')
 
-    if (!checkPreview || !tableOfContents || !contactInformation) {
-      return
-    }
+    if (!checkPreview || !tableOfContents || !contactInformation) return;
 
     checkPreview.classList[scrollY === 0 ? 'remove' : 'add']('cursor-pointer')
+    checkPreview.onclick = () => this.scrollToTop();
 
     if (scrollY < 200) {
-      const scale = 1 - scrollY / 400
-      checkPreview.style.transform = `scale(${scale}) translateY(${scrollY / 2}px)`
-      tableOfContents.style.top = `${445 - scrollY * 0.975}px`
-      contactInformation.style.paddingTop = `${scrollY / 4}px`
-      checkPreview.parentElement.style.width = `${100 - scrollY / 4}%`
+      const tableWidth = tableOfContents.getBoundingClientRect().width;
+      const targetScale = tableWidth / 720;
+
+      const scale = 1 + (targetScale - 1) * (scrollY / 200);
+
+      checkPreview.style.transform = `scale(${scale}) translateY(${scrollY / 2}px)`;
+      contactInformation.style.paddingTop = `${scrollY / 4}px`;
+      checkPreview.parentElement.style.width = `${100 - scrollY / 4}%`;
     } else {
-      checkPreview.style.transform = 'scale(0.5) translateY(100px)'
-      tableOfContents.style.top = `250px`
-      contactInformation.style.paddingTop = `50px`
-      checkPreview.parentElement.style.width = `0%`
+      const tableWidth = tableOfContents.getBoundingClientRect().width;
+      const targetScale = tableWidth / 720;
+
+      checkPreview.style.transform = `scale(${targetScale}) translateY(100px)`;
+      contactInformation.style.paddingTop = `50px`;
+      checkPreview.parentElement.style.width = `0%`;
     }
 
+    tableOfContents.style.top = `${checkPreview.getBoundingClientRect().top + checkPreview.getBoundingClientRect().height + 16}px`;
+
     checkPreview.parentElement.parentElement.style.pointerEvents =
-      scrollY === 0 ? 'auto' : 'none'
+      scrollY === 0 ? 'auto' : 'none';
+    checkPreview.querySelector("div").style.pointerEvents =
+      scrollY === 0 ? 'auto' : 'none';
   }
 }
