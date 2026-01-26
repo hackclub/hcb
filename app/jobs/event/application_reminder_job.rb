73becdbfa7ab53e26d559dfca3f6a4c116a49682
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-01-25 20:22:36 -0800

    Lint

--- app/jobs/event/application_reminder_job.rb
+++ app/jobs/event/application_reminder_job.rb
@@ -5,7 +5,9 @@ module Event
     queue_as :low
     def perform(application, tip_number)
       return unless application.draft?
+
       Event::ApplicationMailer.with(application:, tip_number:).incomplete.deliver_later
     end
+
   end
 end
