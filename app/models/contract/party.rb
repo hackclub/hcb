c25e2e4bc3b514977e1b1fe4829f2cea33a345b8
Samuel Fernandez <me@sfernandez.dev>
2026-01-26 19:37:27 +0000

    Add external_id to parties

--- app/models/contract/party.rb
+++ app/models/contract/party.rb
@@ -13,6 +13,7 @@
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #  contract_id    :bigint           not null
+#  external_id    :string
 #  user_id        :bigint
 #
 # Indexes
