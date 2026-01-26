8bb21a8a7a12432325d542be67daa7d145b35a91
Samuel Fernandez <me@sfernandez.dev>
2026-01-26 19:50:40 +0000

    Backfill in reverse order

--- app/jobs/one_time_jobs/backfill_party_external_ids.rb
+++ app/jobs/one_time_jobs/backfill_party_external_ids.rb
@@ -3,7 +3,7 @@
 module OneTimeJobs
   class BackfillPartyExternalIds < ApplicationJob
     def perform
-      Contract.where(external_service: 0).find_each do |contract|
+      Contract.where(external_service: 0).find_each(order: :desc) do |contract|
         submitters = contract.docuseal_document["submitters"]
 
         next if submitters.nil?
