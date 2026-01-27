e54db51b7d1823a4e18d05c48a4d3e681c3aa286
Samuel Fernandez <me@sfernandez.dev>
2026-01-27 04:44:22 +0000

    Add application mailer preview

---
+++ spec/mailers/previews/event/application_mailer_preview.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Event
+  class ApplicationMailerPreview < ActionMailer::Preview
+    def confirmation
+      Event::ApplicationMailer.with(application: Event::Application.last).confirmation
+    end
+
+    def under_review
+      Event::ApplicationMailer.with(application: Event::Application.last).under_review
+    end
+
+    def incomplete
+      Event::ApplicationMailer.with(application: Event::Application.last).incomplete
+    end
+
+    def rejected
+      Event::ApplicationMailer.with(application: Event::Application.last).rejected
+    end
+  end
+end
