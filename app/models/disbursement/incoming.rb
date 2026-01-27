2d42904eb44e5659a78a7d1512f998bc94c39abb
Albert Chae <albertchae@users.noreply.github.com>
2026-01-26 23:01:11 -0800

    Create Disbursement wrapper classes for incoming/outgoing directions

---
+++ app/models/disbursement/incoming.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Disbursement
+  class Incoming
+    include Base
+
+    def hcb_code
+      disbursement.incoming_hcb_code
+    end
+
+    def event
+      disbursement.destination_event
+    end
+
+    def amount
+      disbursement.amount.abs
+    end
+
+    def subledger
+      disbursement.destination_subledger
+    end
+
+    def transaction_category
+      disbursement.destination_transaction_category
+    end
+  end
+end
---
+++ app/models/disbursement/outgoing.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Disbursement
+  class Outgoing
+    include Base
+
+    def hcb_code
+      disbursement.outgoing_hcb_code
+    end
+
+    def event
+      disbursement.source_event
+    end
+
+    def amount
+      -disbursement.amount.abs
+    end
+
+    def subledger
+      disbursement.source_subledger
+    end
+
+    def transaction_category
+      disbursement.source_transaction_category
+    end
+  end
+end
