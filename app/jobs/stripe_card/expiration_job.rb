352121de1d7ccfb8842c12fc42beae2850538d07
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-01-28 22:05:26 -0500

    Update app/jobs/stripe_card/expiration_job.rb
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/jobs/stripe_card/expiration_job.rb
+++ app/jobs/stripe_card/expiration_job.rb
@@ -4,7 +4,7 @@ class StripeCard
   class ExpirationJob < ApplicationJob
     queue_as :low
     def perform
-      StripeCard.active.or(StripeCard.frozen).find_each do |sc|
+      StripeCard.where.not(stripe_status: "canceled").find_each do |sc|
         if Date.current > Date.new(sc.exp_year, sc.exp_month, 1).end_of_month
           sc.cancel!
         end
