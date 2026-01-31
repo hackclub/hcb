7ead02cd1af75505fa06b96857aa4d6c3942e7f2
Manu Gurudath <manusvathgurudath@gmail.com>
2026-01-30 22:55:23 -0800

    Update app/controllers/concerns/donation_page_setup.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/controllers/concerns/donation_page_setup.rb
+++ app/controllers/concerns/donation_page_setup.rb
@@ -43,7 +43,7 @@ module DonationPageSetup
 
     @monthly = params[:monthly].present? || params[:tier_id].present?
     @skip_layout_og_tags = true
-    
+
     if @monthly
       @recurring_donation = event.recurring_donations.build(
         name: params[:name],
