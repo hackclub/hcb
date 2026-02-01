48cd75bbd6bb89ccd122302a19f271e7ba9155a5
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-02-01 13:22:42 -0800

    Redirect deprecated transactions

--- app/controllers/ledger/items_controller.rb
+++ app/controllers/ledger/items_controller.rb
@@ -6,6 +6,10 @@ class Ledger
       @item = Ledger::Item.find_by_hashid!(params[:id])
 
       authorize @item
+    rescue ActiveRecord::RecordNotFound
+      raise unless Transaction.with_deleted.find_by(id: params[:id]).present? || CanonicalTransaction.find_by(id: params[:id]).present?
+      skip_authorization
+      redirect_to transaction_path(params[:id])
     end
   end
 
