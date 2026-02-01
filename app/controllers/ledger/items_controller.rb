bcce94b0badfc254932aba8c4ca1615d7d7abf28
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-02-01 11:47:58 -0800

    Remove extra controller method

--- app/controllers/ledger/items_controller.rb
+++ app/controllers/ledger/items_controller.rb
@@ -7,23 +7,6 @@ class Ledger
 
       authorize @item
     end
-
-    def comment
-      @item = Ledger::Item.find(params[:id])
-
-      authorize @item
-
-      @item.comments.create!(
-        content: params[:content],
-        file: params[:file],
-        admin_only: params[:admin_only] || false,
-        user: current_user
-      )
-
-      redirect_to params[:redirect_url]
-    rescue => e
-      redirect_to params[:redirect_url], flash: { error: e.message }
-    end
   end
 
 end
