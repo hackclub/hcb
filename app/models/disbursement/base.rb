c624edc3e77cc7184230cd3bd92fa100e1aaa036
Gary Tou <gary@garytou.com>
2026-01-31 17:36:04 -0800

    Revert unnecessary delegrations

--- app/models/disbursement/base.rb
+++ app/models/disbursement/base.rb
@@ -15,9 +15,7 @@ class Disbursement
                :state_text, :state_icon,
                :special_appearance, :special_appearance_name, :special_appearance?,
                :transferred_at, :created_at, :scheduled_on, :errored?, :rejected?,
-               :fulfilled_by, :fee_waived?, :to_param, :special_appearance_memo,
-               :v4_api_state, :outgoing_disbursement, :incoming_disbursement,
-               to: :disbursement
+               :fulfilled_by, :fee_waived?, :to_param, :special_appearance_memo, to: :disbursement
     end
 
     def initialize(disbursement)
