ec9aef6da89bade3e7bd90cde10dba14f0ca99c8
Samuel Fernandez <me@sfernandez.dev>
2026-02-03 02:35:45 +0000

    Fix affiliation policy

--- app/policies/event/affiliation_policy.rb
+++ app/policies/event/affiliation_policy.rb
@@ -11,13 +11,13 @@ class Event
     def update?
       return true if user.admin?
       return OrganizerPosition.role_at_least?(user, record.affiliable, :manager) if record.affiliable.is_a?(Event)
-      return user == record.user if record.is_a?(Event::Application)
+      return user == record.affiliable.user if record.affiliable.is_a?(Event::Application)
     end
 
     def destroy?
       return true if user.admin?
       return OrganizerPosition.role_at_least?(user, record.affiliable, :manager) if record.affiliable.is_a?(Event)
-      return user == record.user if record.is_a?(Event::Application)
+      return user == record.affiliable.user if record.affiliable.is_a?(Event::Application)
     end
 
   end
