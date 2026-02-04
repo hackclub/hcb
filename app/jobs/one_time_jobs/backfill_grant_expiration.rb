43a265813e8ffacd9891231f537322d209e1d46c
Gary Tou <gary@garytou.com>
2026-02-04 11:42:00 -0800

    Remove fallback

--- app/jobs/one_time_jobs/backfill_grant_expiration.rb
+++ app/jobs/one_time_jobs/backfill_grant_expiration.rb
@@ -4,7 +4,7 @@ module OneTimeJobs
   class BackfillGrantExpiration
     def self.perform
       CardGrant.find_each do |cg|
-        cg.update(expiration_at: cg.created_at + (CardGrantSetting.expiration_preferences[cg.event.card_grant_setting&.expiration_preference].days || 365.days))
+        cg.update(expiration_at: cg.created_at + (CardGrantSetting.expiration_preferences[cg.event.card_grant_setting.expiration_preference].days))
       end
     end
 
