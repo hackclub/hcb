91347ca1063ff9aad629d8f942837cdc637b8ef6
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-02-06 20:59:00 +0000

    Fix migration

--- db/migrate/20260206201808_change_referral_attributions_index.rb
+++ db/migrate/20260206201808_change_referral_attributions_index.rb
@@ -3,12 +3,11 @@ class ChangeReferralAttributionsIndex < ActiveRecord::Migration[8.0]
 
   def change
     remove_index :referral_attributions,
-                 name: "index_referral_attributions_on_user_id_and_referral_program_id"
+                 [:user_id, :referral_program_id]
 
     add_index :referral_attributions,
               [:user_id, :referral_link_id],
               unique: true,
-              name: "index_referral_attributions_on_user_id_and_referral_link_id",
               algorithm: :concurrently
   end
 end
