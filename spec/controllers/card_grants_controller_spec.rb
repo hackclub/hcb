1ba6e7b8b235f08f9e7ac47e5fc38a1763e09d0c
yodalightsabr <yoda@yodacode.xyz>
2026-02-12 16:59:01 -0800

    Fix tests

--- spec/controllers/card_grants_controller_spec.rb
+++ spec/controllers/card_grants_controller_spec.rb
@@ -2,48 +2,14 @@
 
 require "rails_helper"
 
-RSpec.describe CardGrantsController do
-  include SessionSupport
+RSpec.describe Api::V4::CardGrantsController do
   render_views
-
-  describe "#new" do
-    it "renders successfully" do
-      user = create(:user)
-      event = create(:event)
-      create(:organizer_position, user:, event:)
-      sign_in(user)
-
-      expect(event.card_grant_setting).to be_nil
-
-      get(:new, params: { event_id: event.friendly_id })
-
-      expect(response).to have_http_status(:ok)
-      expect(event.reload.card_grant_setting).to be_present
-    end
-
-    it "uses the email param to pre-fill the email field" do
-      user = create(:user)
-      event = create(:event)
-      create(:organizer_position, user:, event:)
-      sign_in(user)
-
-      expect(event.card_grant_setting).to be_nil
-
-      get(:new, params: { event_id: event.friendly_id, email: "orpheus@hackclub.com" })
-
-      expect(response).to have_http_status(:ok)
-      expect(event.reload.card_grant_setting).to be_present
-      input = response.parsed_body.css("[name='card_grant[email]']").sole
-      expect(input.get_attribute("value")).to eq("orpheus@hackclub.com")
-    end
-  end
 
   describe "#create" do
     def card_grant_params
       {
-        amount_cents: "123.45",
+        amount_cents: "123_45",
         email: "recipient@example.com",
-        invite_message: "this is a really cool card grant",
         keyword_lock: "some keywords",
         purpose: "Raffle prize",
         one_time_use: "true",
@@ -53,143 +19,149 @@ RSpec.describe CardGrantsController do
     end
 
     it "creates a card grant" do
-      user = create(:user)
-      event = create(:event, :with_positive_balance, plan_type: Event::Plan::HackClubAffiliate)
+      user = create(:user, full_name: "Orpheus the Dinosaur", email: "orpheus@hackclub.com")
+      event = create(:event, :with_positive_balance, name: "Test Event", plan_type: Event::Plan::HackClubAffiliate)
       create(:card_grant_setting, event:)
       create(:organizer_position, user:, event:)
-      sign_in(user)
 
-      post(
-        :create,
-        params: {
-          event_id: event.friendly_id,
-          card_grant: card_grant_params,
-        }
-      )
+      token = create(:api_token, user:)
+      request.headers["Authorization"] = "Bearer #{token.token}"
 
-      expect(response).to redirect_to(event_transfers_path(event))
+      # `UsersHelper#profile_picture_for` uses `gravatar_url` if the user
+      # hasn't uploaded an image. The background colour for the Gravatar
+      # fallback image is determined by the user's ID, which makes the
+      # response value unpredictable.
+      allow_any_instance_of(UsersHelper).to receive(:gravatar_url).and_return("https://gravatar.com/avatar/stubbed")
+
+      post(:create, params: { event_id: event.friendly_id, **card_grant_params, expand: "disbursements" }, as: :json)
+
+      expect(response).to have_http_status(:created)
       card_grant = event.card_grants.sole
-      expect(card_grant.amount_cents).to eq(123_45)
-      expect(card_grant.email).to eq("recipient@example.com")
-      expect(card_grant.keyword_lock).to eq("some keywords")
-      expect(card_grant.purpose).to eq("Raffle prize")
-      expect(card_grant.one_time_use).to eq(true)
-      expect(card_grant.pre_authorization_required).to eq(true)
-      expect(card_grant.instructions).to eq("Here's a card grant for your raffle prize")
+      disbursement = card_grant.disbursement
+      recipient = card_grant.user
+
+      serialized_event = {
+        "id"                                => event.public_id,
+        "parent_id"                         => nil,
+        "name"                              => "Test Event",
+        "slug"                              => "test-event",
+        "background_image"                  => nil,
+        "country"                           => nil,
+        "created_at"                        => event.created_at.iso8601(3),
+        "fee_percentage"                    => 0.0,
+        "financially_frozen"                => false,
+        "icon"                              => nil,
+        "donation_page_available"           => true,
+        "playground_mode"                   => false,
+        "playground_mode_meeting_requested" => false,
+        "transparent"                       => true
+      }
+
+      expect(response.parsed_body).to eq(
+        {
+          "id"                         => card_grant.public_id,
+          "amount_cents"               => 123_45,
+          "card_id"                    => nil,
+          "one_time_use"               => true,
+          "pre_authorization_required" => true,
+          "status"                     => "active",
+          "allowed_categories"         => [],
+          "allowed_merchants"          => [],
+          "category_lock"              => [],
+          "merchant_lock"              => [],
+          "purpose"                    => "Raffle prize",
+          "keyword_lock"               => "some keywords",
+          "email"                      => "recipient@example.com",
+          "expires_on"                 => card_grant.expires_on.iso8601(3),
+          "disbursements"              => [
+            {
+              "id"                      => disbursement.public_id,
+              "memo"                    => "Grant to recipient",
+              "status"                  => "completed",
+              "transaction_id"          => disbursement.local_hcb_code.public_id,
+              "outgoing_transaction_id" => disbursement.outgoing_disbursement.local_hcb_code.public_id,
+              "incoming_transaction_id" => disbursement.incoming_disbursement.local_hcb_code.public_id,
+              "amount_cents"            => 123_45,
+              "card_grant_id"           => card_grant.public_id,
+              "from"                    => serialized_event,
+              "to"                      => serialized_event,
+              "sender"                  => {
+                "id"       => user.public_id,
+                "name"     => "Orpheus D",
+                "email"    => "orpheus@hackclub.com",
+                "admin"    => false,
+                "auditor"  => false,
+                "avatar"   => "https://gravatar.com/avatar/stubbed",
+                "birthday" => nil,
+              },
+            }
+          ],
+          "organization"               => serialized_event,
+          "user"                       => {
+            "id"      => recipient.public_id,
+            "name"    => "recipient",
+            "admin"   => false,
+            "auditor" => false,
+            "avatar"  => "https://gravatar.com/avatar/stubbed",
+          },
+        }
+      )
     end
 
-    it "handles validation errors" do
-      user = create(:user)
-      event = create(:event, :with_positive_balance, plan_type: Event::Plan::HackClubAffiliate)
+    it "reports validation errors" do
+      user = create(:user, full_name: "Orpheus the Dinosaur", email: "orpheus@hackclub.com")
+      event = create(:event, :with_positive_balance, name: "Test Event", plan_type: Event::Plan::HackClubAffiliate)
       create(:card_grant_setting, event:)
       create(:organizer_position, user:, event:)
-      sign_in(user)
+
+      token = create(:api_token, user:)
+      request.headers["Authorization"] = "Bearer #{token.token}"
 
       post(
         :create,
         params: {
           event_id: event.friendly_id,
-          card_grant: {
-            **card_grant_params,
-            purpose: "This is a very long purpose that should exceed the 30 character limit"
-          }
-        }
+          **card_grant_params,
+          purpose: "This is a very long purpose that should exceed the 30 character limit",
+        },
+        as: :json
       )
 
-      expect(event.card_grants).to be_empty
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(flash[:error]).to eq("Purpose is too long (maximum is 30 characters)")
+      expect(response.parsed_body).to eq(
+        {
+          "error"    => "invalid_operation",
+          "messages" => ["Purpose is too long (maximum is 30 characters)"]
+        }
+      )
     end
 
     it "handles downstream errors" do
-      user = create(:user)
-      event = create(:event, :with_positive_balance, plan_type: Event::Plan::HackClubAffiliate)
+      user = create(:user, full_name: "Orpheus the Dinosaur", email: "orpheus@hackclub.com")
+      event = create(:event, :with_positive_balance, name: "Test Event", plan_type: Event::Plan::HackClubAffiliate)
       create(:card_grant_setting, event:)
       create(:organizer_position, user:, event:)
-      sign_in(user)
+
+      token = create(:api_token, user:)
+      request.headers["Authorization"] = "Bearer #{token.token}"
 
       post(
         :create,
         params: {
           event_id: event.friendly_id,
-          card_grant: {
-            **card_grant_params,
-            amount_cents: "12345.67",
-          }
-        }
+          **card_grant_params,
+          amount_cents: 12_345_67,
+        },
+        as: :json
       )
 
-      expect(event.card_grants).to be_empty
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(flash[:error]).to eq("You don't have enough money to make this disbursement.")
-    end
-  end
-
-  describe "topup" do
-    it "tops up a card grant" do
-      user = create(:user)
-      event = create(:event, :with_positive_balance, plan_type: Event::Plan::HackClubAffiliate)
-      create(:organizer_position, user:, event:)
-      sign_in(user)
-
-      card_grant = create(
-        :card_grant,
-        event:,
-        sent_by: user,
-        amount_cents: 12_34
-      )
-
-      post(
-        :topup,
-        params: {
-          event_id: event.friendly_id,
-          id: card_grant.hashid,
-          amount: "56.78"
+      expect(response.parsed_body).to eq(
+        {
+          "error"    => "invalid_operation",
+          "messages" => ["You don't have enough money to make this disbursement."]
         }
       )
-
-      expect(flash[:success]).to eq("Successfully topped up grant.")
-      expect(response).to redirect_to(card_grant_path(card_grant))
-
-      expect(card_grant.reload.balance).to eq(Money.new(69_12, :usd))
-
-      disbursement = event.disbursements.last
-      expect(disbursement.amount).to eq(56_78)
-      expect(disbursement.source_event).to eq(event)
-      expect(disbursement.event).to eq(event)
-      expect(disbursement.source_subledger_id).to be_nil
-      expect(disbursement.destination_subledger_id).to eq(card_grant.subledger_id)
-      expect(disbursement.requested_by_id).to eq(user.id)
-    end
-
-    it "handles downstream errors" do
-      user = create(:user)
-      event = create(:event, :with_positive_balance, plan_type: Event::Plan::HackClubAffiliate)
-      create(:organizer_position, user:, event:)
-      sign_in(user)
-
-      card_grant = create(
-        :card_grant,
-        event:,
-        sent_by: user,
-        amount_cents: 12_34
-      )
-
-      expect do
-        post(
-          :topup,
-          params: {
-            event_id: event.friendly_id,
-            id: card_grant.hashid,
-            amount: "12345.67"
-          }
-        )
-      end.not_to change(event.disbursements, :count)
-
-      expect(flash[:error]).to eq("You don't have enough money to make this disbursement.")
-      expect(response).to redirect_to(card_grant_path(card_grant))
-
-      expect(card_grant.reload.balance).to eq(Money.new(12_34, :usd))
     end
   end
 end
