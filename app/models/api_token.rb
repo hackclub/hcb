6896393eadde6ab9e2741d012bcc3c2486737c04
Manu Gurudath <manusvathgurudath@gmail.com>
2026-02-14 00:16:39 +0000

    Encrypt refresh tokens in ApiTokens model and update schema

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -4,25 +4,27 @@
 #
 # Table name: api_tokens
 #
-#  id               :bigint           not null, primary key
-#  expires_in       :integer
-#  ip_address       :inet
-#  refresh_token    :string
-#  revoked_at       :datetime
-#  scopes           :string
-#  token_bidx       :string
-#  token_ciphertext :text
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  application_id   :bigint
-#  user_id          :bigint           not null
+#  id                       :bigint           not null, primary key
+#  expires_in               :integer
+#  ip_address               :inet
+#  refresh_token_bidx       :string
+#  refresh_token_ciphertext :text
+#  revoked_at               :datetime
+#  scopes                   :string
+#  token_bidx               :string
+#  token_ciphertext         :text
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  application_id           :bigint
+#  user_id                  :bigint           not null
 #
 # Indexes
 #
-#  index_api_tokens_on_application_id  (application_id)
-#  index_api_tokens_on_ip_address      (ip_address)
-#  index_api_tokens_on_token_bidx      (token_bidx) UNIQUE
-#  index_api_tokens_on_user_id         (user_id)
+#  index_api_tokens_on_application_id      (application_id)
+#  index_api_tokens_on_ip_address          (ip_address)
+#  index_api_tokens_on_refresh_token_bidx  (refresh_token_bidx) UNIQUE
+#  index_api_tokens_on_token_bidx          (token_bidx) UNIQUE
+#  index_api_tokens_on_user_id             (user_id)
 #
 # Foreign Keys
 #
@@ -45,6 +47,9 @@ class ApiToken < ApplicationRecord
 
   has_encrypted :token
   blind_index :token
+
+  has_encrypted :refresh_token
+  blind_index :refresh_token
 
   belongs_to :user
 
