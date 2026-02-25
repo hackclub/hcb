4f6519228a3cc2c2189434affe13b6e67b7fbf2c
rluo.dev <34289582+rluodev@users.noreply.github.com>
2026-02-25 16:50:33 -0500

    make ach transfer aasm column not null

---
+++ db/migrate/20260225214743_make_ach_transfer_aasm_state_not_null.rb
@@ -0,0 +1,10 @@
+class MakeAchTransferAasmStateNotNull < ActiveRecord::Migration[8.0]
+  def up
+    safety_assured do
+      change_column_null :ach_transfers, :aasm_state, false
+    end
+  end
+  def down
+    change_column_null :ach_transfers, :aasm_state, true
+  end
+end
