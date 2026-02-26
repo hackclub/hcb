86da9cba59ee50bc437b20372d732a0f0ede99b0
Gary Tou <gary@garytou.com>
2026-02-26 00:31:51 -0800

    Apply validations to models

--- config/initializers/nondisposable.rb
+++ config/initializers/nondisposable.rb
@@ -2,11 +2,11 @@
 
 Nondisposable.configure do |config|
   # Customize the error message if needed
-  # config.error_message = "is not allowed. Please use a non-disposable email address."
-  #
+  config.error_message = "provider is unsupported. Please try with another email address."
+
   # Add custom domains you want to be considered as disposable
-  # config.additional_domains = ['custom-disposable-domain.com']
-  #
+  config.additional_domains = ["aboodbab.com"]
+
   # Exclude domains that are considered disposable but you want to allow anyways
-  # config.excluded_domains = ['false-positive-domain.com']
+  config.excluded_domains = ['false-positive-domain.com']
 end
