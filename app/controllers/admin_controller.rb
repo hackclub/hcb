56ae27ed310f2c78dea844a051762862a570bb6a
Gary Tou <gary@garytou.com>
2026-02-28 21:19:01 -0800

    Actually, sort reimbursement_requested_at by ASC

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -495,7 +495,8 @@ class AdminController < Admin::BaseController
     @reports = relation.page(@page).per(@per).order(
       @unprocessed_wise_report_ids.any? ? Arel.sql("CASE WHEN reimbursement_reports.id IN (#{@unprocessed_wise_report_ids.join(',')}) THEN 1 ELSE 0 END DESC") : nil,
       Arel.sql("reimbursement_reports.aasm_state = 'reimbursement_requested' DESC"),
-      Arel.sql("COALESCE(reimbursement_reports.reimbursement_requested_at, reimbursement_reports.created_at) DESC")
+      Arel.sql("reimbursement_reports.reimbursement_requested_at ASC NULLS LAST"),
+      Arel.sql("reimbursement_reports.created_at DESC")
     )
 
   end
