1bb105d62ddbad888fcfbd379e1b377cd175dae3
Echo <github@3kh0.net>
2026-03-05 09:17:05 -0500

    [v3 API] expose helpful details on card charges

--- app/api/api/entities/card_charge.rb
+++ app/api/api/entities/card_charge.rb
@@ -11,6 +11,51 @@ module Api
           expose :date
         end
 
+        expose :status, documentation: {
+          type: "string",
+          values: %w[pending closed reversed declined]
+        } do |hcb_code|
+          if hcb_code.pt&.declined?
+            "declined"
+          elsif hcb_code.stripe_reversed_by_merchant?
+            "reversed"
+          elsif hcb_code.canonical_transactions.any?
+            "closed"
+          else
+            "pending"
+          end
+        end
+
+        expose :declined, documentation: { type: "boolean" } do |hcb_code|
+          hcb_code.pt&.declined? || false
+        end
+
+        expose :decline_reason, documentation: { type: "string" } do |hcb_code|
+          hcb_code.pt&.decline_reason&.to_s
+        end
+
+        expose :merchant do
+          expose :merchant_name, as: :name, documentation: { type: "string" } do |hcb_code|
+            hcb_code.stripe_merchant&.dig("name")
+          end
+          expose :merchant_category, as: :category, documentation: { type: "string" } do |hcb_code|
+            hcb_code.stripe_merchant&.dig("category")
+          end
+          expose :merchant_country, as: :country, documentation: { type: "string" } do |hcb_code|
+            hcb_code.stripe_merchant&.dig("country")
+          end
+          expose :merchant_network_id, as: :network_id, documentation: { type: "string" } do |hcb_code|
+            hcb_code.stripe_merchant&.dig("network_id")
+          end
+        end
+
+        expose :authorization_method, documentation: {
+          type: "string",
+          values: %w[keyed_in swipe chip contactless online]
+        } do |hcb_code|
+          hcb_code.pt&.raw_pending_stripe_transaction&.stripe_transaction&.dig("authorization_method")
+        end
+
         expose_associated Card, hide: [Card, Organization, User] do |hcb_code, options|
           hcb_code.stripe_card
         end
