a9d4f8db195874758a8bbea31bb14680700946da
Manu Gurudath <manusvathgurudath@gmail.com>
2026-03-06 17:27:58 +0000

    Add recipient name field to payout methods and update forms

--- db/schema.rb
+++ db/schema.rb
@@ -2514,6 +2514,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_12_205901) do
   create_table "user_payout_method_ach_transfers", force: :cascade do |t|
     t.text "account_number_ciphertext", null: false
     t.datetime "created_at", null: false
+    t.string "recipient_name"
     t.text "routing_number_ciphertext", null: false
     t.datetime "updated_at", null: false
   end
@@ -2526,12 +2527,14 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_12_205901) do
     t.text "address_postal_code", null: false
     t.text "address_state", null: false
     t.datetime "created_at", null: false
+    t.string "recipient_name"
     t.datetime "updated_at", null: false
   end
 
   create_table "user_payout_method_paypal_transfers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "recipient_email", null: false
+    t.string "recipient_name"
     t.datetime "updated_at", null: false
   end
 
@@ -2563,6 +2566,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_12_205901) do
     t.string "currency"
     t.integer "recipient_country"
     t.text "recipient_information_ciphertext"
+    t.string "recipient_name"
     t.datetime "updated_at", null: false
     t.text "wise_recipient_id"
   end
