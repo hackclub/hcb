99638ef1faa143e767a7740bcfce74f01d4ff03f
Manu Gurudath <manusvathgurudath@gmail.com>
2026-03-07 02:05:56 +0000

    Remove redundant constraint migrations
    Instead of adding a NOT NULL constraint and then immediately removing it, we just keep the columns nullable for a smooth rollout.

--- db/migrate/20260301000003_add_constraints_to_first_last_names.rb
+++
@@ -1,6 +0,0 @@
-class AddConstraintsToFirstLastNames < ActiveRecord::Migration[8.0]
-  def change
-    add_check_constraint :users, "first_name IS NOT NULL", name: "users_first_name_not_null", validate: false
-    # Note: last_name can be NULL to support single-name users
-  end
-end
--- db/migrate/20260301000004_remove_not_null_constraint_from_first_name.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotNullConstraintFromFirstName < ActiveRecord::Migration[8.0]
-  def change
-    remove_check_constraint :users, name: "users_first_name_not_null"
-  end
-end
