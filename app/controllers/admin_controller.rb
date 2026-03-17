7b96ed7306d8f148c57a4a60ba1006d4ac88c6de
Manu Gurudath <manu@Manus-MacBook-Pro.local>
2026-03-17 12:59:16 -0700

    Add to pending ledger

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -393,9 +393,9 @@ class AdminController < Admin::BaseController
   def user_search
     @q = params[:q]
     @users = if @q
-              User.where("full_name ILIKE ? OR email ILIKE ? OR CAST(id AS TEXT) ILIKE ?", "%#{User.sanitize_sql_like(@q)}%", "%#{User.sanitize_sql_like(@q)}%", "%#{User.sanitize_sql_like(@q)}%").order(:full_name).limit(20).select(:id, :full_name, :email)
+               User.where("full_name ILIKE ? OR email ILIKE ? OR CAST(id AS TEXT) ILIKE ?", "%#{User.sanitize_sql_like(@q)}%", "%#{User.sanitize_sql_like(@q)}%", "%#{User.sanitize_sql_like(@q)}%").order(:full_name).limit(20).select(:id, :full_name, :email)
              else
-              User.none
+               User.none
              end
     render turbo_stream: helpers.async_combobox_options(@users)
   end
