2f5b08f0650f53ae4b54d7c1752159a61dde8ad9
Albert Chae <albertchae@users.noreply.github.com>
2026-03-19 08:00:35 -0700

    Add more test cases for comment_policy_spec

--- spec/policies/comment_policy_spec.rb
+++ spec/policies/comment_policy_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe CommentPolicy, type: :policy do
   end
 
   describe "#users" do
-    context "when commentable responds to :events" do
+    context "when commentable responds to :events (HcbCode)" do
       it "includes both direct users and ancestor users" do
         parent_event = create(:event)
         ancestor_user = create(:user)
@@ -22,43 +22,107 @@ RSpec.describe CommentPolicy, type: :policy do
         hcb_code = create_hcb_code_for_event(child_event)
         comment = create(:comment, commentable: hcb_code)
         policy = described_class.new(direct_user, comment)
-        policy_users = policy.send(:users)
 
-        expect(policy_users).to include(direct_user)
-        expect(policy_users).to include(ancestor_user)
+        expect(policy.send(:users)).to include(direct_user, ancestor_user)
+      end
+    end
+
+    context "when commentable is a Reimbursement::Report" do
+      it "includes the report user and event organizers" do
+        event = create(:event)
+        organizer = create(:user)
+        create(:organizer_position, event: event, user: organizer)
+        report_user = create(:user)
+        report = create(:reimbursement_report, user: report_user, event: event)
+
+        comment = create(:comment, commentable: report)
+        policy = described_class.new(report_user, comment)
+
+        expect(policy.send(:users)).to include(report_user, organizer)
+      end
+    end
+
+    context "when commentable is a Disbursement" do
+      it "includes users from both source and destination events" do
+        source_user = create(:user)
+        destination_user = create(:user)
+        disbursement = create(:disbursement)
+        create(:organizer_position, event: disbursement.source_event, user: source_user)
+        create(:organizer_position, event: disbursement.destination_event, user: destination_user)
+
+        comment = create(:comment, commentable: disbursement)
+        policy = described_class.new(source_user, comment)
+
+        expect(policy.send(:users)).to include(source_user, destination_user)
+      end
+    end
+
+    context "when commentable is an Event" do
+      it "returns no users" do
+        event = create(:event)
+        user = create(:user)
+        create(:organizer_position, event: event, user: user)
+
+        comment = create(:comment, commentable: event)
+        policy = described_class.new(user, comment)
+
+        expect(policy.send(:users)).to be_empty
+      end
+    end
+
+    context "when commentable falls through to the default branch (AchTransfer)" do
+      it "returns the event's users" do
+        event = create(:event, :with_positive_balance)
+        user = create(:user)
+        create(:organizer_position, event: event, user: user)
+        ach_transfer = create(:ach_transfer, event: event)
+
+        comment = create(:comment, commentable: ach_transfer)
+        policy = described_class.new(user, comment)
+
+        expect(policy.send(:users)).to include(user)
+      end
+    end
+
+    context "when commentable responds to :author" do
+      it "includes the author" do
+        author = create(:user)
+        disbursement = create(:disbursement, requested_by: author)
+        hcb_code = disbursement.outgoing_disbursement.local_hcb_code
+
+        comment = create(:comment, commentable: hcb_code)
+        policy = described_class.new(author, comment)
+
+        expect(policy.send(:users)).to include(author)
       end
     end
   end
 
   describe "#show?" do
-    context "when commentable responds to :events" do
-      it "allows organizers of the event to view comments" do
-        event = create(:event)
-        user = create(:user)
-        create(:organizer_position, event: event, user: user)
+    it "allows organizers of the event to view comments" do
+      event = create(:event)
+      user = create(:user)
+      create(:organizer_position, event: event, user: user)
 
-        hcb_code = create_hcb_code_for_event(event)
-        comment = create(:comment, commentable: hcb_code, admin_only: false)
-        policy = described_class.new(user, comment)
+      hcb_code = create_hcb_code_for_event(event)
+      comment = create(:comment, commentable: hcb_code, admin_only: false)
+      policy = described_class.new(user, comment)
 
-        expect(policy.show?).to eq(true)
-      end
+      expect(policy.show?).to eq(true)
     end
   end
 
   describe "#create?" do
-    context "when commentable responds to :events" do
-      it "allows organizers to create comments" do
-        event = create(:event)
-        user = create(:user)
-        create(:organizer_position, event: event, user: user)
+    it "allows organizers to create comments" do
+      event = create(:event)
+      user = create(:user)
+      create(:organizer_position, event: event, user: user)
 
-        hcb_code = create_hcb_code_for_event(event)
-        comment = build(:comment, commentable: hcb_code, admin_only: false)
-        policy = described_class.new(user, comment)
+      hcb_code = create_hcb_code_for_event(event)
+      comment = build(:comment, commentable: hcb_code, admin_only: false)
+      policy = described_class.new(user, comment)
 
-        expect(policy.create?).to eq(true)
-      end
+      expect(policy.create?).to eq(true)
     end
   end
 end
