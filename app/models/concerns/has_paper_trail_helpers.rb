d45cb43d139da455edc14b4986b04f9bf22825e3
Manu Gurudath <manusvathgurudath@gmail.com>
2026-03-23 11:16:45 -0700

    Update has_paper_trail_helpers.rb

--- app/models/concerns/has_paper_trail_helpers.rb
+++ app/models/concerns/has_paper_trail_helpers.rb
@@ -6,7 +6,7 @@ module HasPaperTrailHelpers
   def last_user_change_to(...)
     user_id = versions.where_object_changes_to(...).last&.whodunnit
 
-    return nil if user_id.blank? || !user_id.to_s.match?(/\A\d+\z/)
+    user_id && User.find(user_id)
 
     User.find_by(id: user_id)
   end
