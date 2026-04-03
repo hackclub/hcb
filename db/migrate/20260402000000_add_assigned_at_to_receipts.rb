bb79b97e2aed8e97414a0dab3c68d354af02f26d
Manu Gurudath <manusvathgurudath@gmail.com>
2026-04-02 22:03:22 -0700

    Update 20260402000000_add_assigned_at_to_receipts.rb

--- db/migrate/20260402000000_add_assigned_at_to_receipts.rb
+++ db/migrate/20260402000000_add_assigned_at_to_receipts.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 class AddAssignedAtToReceipts < ActiveRecord::Migration[8.0]
-  def up
+  def change
     add_column :receipts, :assigned_at, :datetime
 
-    execute <<~SQL
-      UPDATE receipts
-      SET assigned_at = created_at
-      WHERE receiptable_id IS NOT NULL
-        AND assigned_at IS NULL
-    SQL
-  end
-
-  def down
-    remove_column :receipts, :assigned_at
-  end
+    reversible do |dir|
+      dir.up do
+        execute <<~SQL
+          UPDATE receipts
+          SET assigned_at = created_at
+          WHERE receiptable_id IS NOT NULL
+            AND assigned_at IS NULL
+        SQL
+      end
+    end
 end
