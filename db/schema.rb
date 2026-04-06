d87623ebce80add058fddeb7a6aa8808178ab8a8
rluo.dev <34289582+rluodev@users.noreply.github.com>
2026-04-06 22:00:42 +0000

    do migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_31_024739) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_06_215433) do
   create_schema "google_sheets"
 
   # These are extensions that must be enabled in order to support this database
@@ -791,7 +791,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_31_024739) do
   end
 
   create_table "donations", force: :cascade do |t|
-    t.string "aasm_state"
+    t.string "aasm_state", null: false
     t.integer "amount"
     t.integer "amount_received"
     t.boolean "anonymous", default: false, null: false
