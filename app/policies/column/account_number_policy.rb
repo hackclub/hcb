a2c9cb56bfc8c25621d79139f1b9ec4b382463ec
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-04-08 14:30:19 -0400

    Apply suggestions from code review
    Co-authored-by: Gary Tou <gary@garytou.com>
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>

--- app/policies/column/account_number_policy.rb
+++ app/policies/column/account_number_policy.rb
@@ -3,6 +3,7 @@
 module Column
   class AccountNumberPolicy < ApplicationPolicy
     def create?
+      # Usually, we don't allow auditors to create. However, this is needed for account numbers because we create them on-demand during page load.
       user&.auditor? || OrganizerPosition.role_at_least?(user, record.event, :manager)
     end
 
