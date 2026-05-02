caafc52b80d7d8bf3791b26f04c5e63be21f2f56
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-05-02 16:58:29 -0500

    Add admin-only querying frontend

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -5,7 +5,21 @@ class LedgersController < ApplicationController
     @ledger = Ledger.find_by_hashid!(params[:id])
     authorize @ledger
 
-    @items = Ledger::Query.new({}).execute(ledgers: [@ledger]).order(date: :desc, id: :desc).page(params[:page])
+    query_hash = {}
+    if auditor_signed_in? && params[:query].present?
+      begin
+        query_hash = JSON.parse(params[:query])
+      rescue JSON::ParserError => e
+        flash.now[:error] = "Invalid query JSON: #{e.message}"
+      end
+    end
+
+    begin
+      @items = Ledger::Query.new(query_hash).execute(ledgers: [@ledger]).order(date: :desc, id: :desc).page(params[:page])
+    rescue Ledger::Query::Error => e
+      flash.now[:error] = "Query error: #{e.message}"
+      @items = Ledger::Query.new({}).execute(ledgers: [@ledger]).order(date: :desc, id: :desc).page(params[:page])
+    end
   end
 
 end
