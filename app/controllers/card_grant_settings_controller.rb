66781d6ffc1fdab00fffc4220ed97f2b3f6fd483
Sarvesh <78450215+Sarvesh-Mk@users.noreply.github.com>
2026-05-11 22:42:58 +0000

    moved card grant settings controller file

---
+++ app/controllers/card_grant_settings_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CardGrant
+  class CardGrantController < ApplicationController
+    before_action :set_card_grant
+
+    def update
+      pass
+    end
+
+  end
+
+end
