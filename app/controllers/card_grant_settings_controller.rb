1353b0ab9413a615bf31a8cbd3100e881dfb790d
Sarvesh <78450215+Sarvesh-Mk@users.noreply.github.com>
2026-05-11 23:34:41 +0000

    routed card grant settings through its own controller

--- app/controllers/card_grant_settings_controller.rb
+++ app/controllers/card_grant_settings_controller.rb
@@ -2,6 +2,28 @@
 
 class CardGrantSettingsController < ApplicationController
   def update
+    card_grant_setting = CardGrantSetting.find(params[:id])
+    authorize card_grant_setting
+    card_grant_setting.update!(card_grant_setting_params)
+  end
+
+  private
+
+  def card_grant_setting_params
+    params.permit(
+      :merchant_lock,
+      :category_lock,
+      :keyword_lock,
+      :invite_message,
+      :banned_merchants,
+      :banned_categories,
+      :expiration_preference,
+      :reimbursement_conversions_enabled,
+      :pre_authorization_required,
+      :block_suspected_fraud,
+      :support_message,
+      :support_url
+    )
   end
 
 end
--- app/policies/card_grant_setting_policy.rb
+++ app/policies/card_grant_setting_policy.rb
@@ -2,6 +2,7 @@
 
 class CardGrantSettingPolicy < ApplicationPolicy
   def update?
+    user&.admin? || OrganizerPosition.role_at_least?(user, record.event, :manager)
   end
 
 end
