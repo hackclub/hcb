0d69c0dd8acd88535e1f05b633e2cd8349dcfff7
Mohamad Mortada <mmortada685@gmail.com>
2026-05-21 17:19:50 -0700

    use trusted oauth app token for rpsec

--- spec/controllers/api/v4/donations_controller_spec.rb
+++ spec/controllers/api/v4/donations_controller_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe Api::V4::DonationsController do
       event = create(:event)
       create(:organizer_position, user:, event:)
 
-      token = create(:api_token, user:)
+      trusted_app = Doorkeeper::Application.create!(name: "Trusted App", redirect_uri: "", trusted: true)
+      token = create(:api_token, user:, application: trusted_app)
       request.headers["Authorization"] = "Bearer #{token.token}"
 
       message = "Thanks for the great work — keep it up!"
