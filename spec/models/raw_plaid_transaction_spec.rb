a4393ef681b58851565a47fd54fb58bfdea3e207
Samuel Fernandez <me@sfernandez.dev>
2026-05-28 11:20:22 -0400

    Upgrade money gems to latest versions

--- spec/models/raw_plaid_transaction_spec.rb
+++ spec/models/raw_plaid_transaction_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe RawPlaidTransaction, type: :model do
     it "uses money gem to display amount" do
       raw_plaid_transaction = create(:raw_plaid_transaction, amount_cents: 100)
 
-      expect(raw_plaid_transaction.amount).to eql(Money.new(100))
+      expect(raw_plaid_transaction.amount).to eql(Money.new(100, "USD"))
     end
   end
 end
