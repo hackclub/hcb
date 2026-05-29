7e4c0212160a6f0ebc0e7a04520b2f974200f7ad
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-05-29 18:55:37 +0000

    Add to receipt report

--- app/mailers/receiptable_mailer.rb
+++ app/mailers/receiptable_mailer.rb
@@ -16,6 +16,8 @@ class ReceiptableMailer < ApplicationMailer
 
     @show_org = @user.events.size > 1
 
+    @delivery_reason = "spent funds with an HCB Visa® Commercial card and are required to upload receipts for all funds spent. HCB Visa® Commercial cards are powered by Stripe and issued by Celtic Bank."
+
     mail
   end
 
