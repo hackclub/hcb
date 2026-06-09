f432f7fe0cbbb825687378e6458ea75ecc86a84a
Samuel Fernandez <me@sfernandez.dev>
2026-06-09 15:14:36 -0400

    Handle custom response times

--- app/models/event/application.rb
+++ app/models/event/application.rb
@@ -288,8 +288,8 @@ class Event
       fs_contract
     end
 
-    def response_time
-      teen_led? ? "2 business days" : "2 weeks"
+    def response_business_days
+      teen_led? ? 2 : 10
     end
 
     def status_color
