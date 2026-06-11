dd3727817a1e3aaa8ec93969632646db99101b6c
Samuel Fernandez <me@sfernandez.dev>
2026-06-10 23:41:59 -0400

    Add back event.approved? condition

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -225,19 +225,19 @@ module EventsHelper
           name: "Donations",
           path_proc: ->(event_id) { edit_event_path(event_id, tab: "donations") },
           symbol: :settings_donations,
-          available_proc: ->(event) { event.plan.donations_enabled? }
+          available_proc: ->(event) { event.approved? && event.plan.donations_enabled? }
         },
         {
           name: "Reimbursements",
           path_proc: ->(event_id) { edit_event_path(event_id, tab: "reimbursements") },
           symbol: :settings_reimbursements,
-          available_proc: ->(event) { event.plan.reimbursements_enabled? }
+          available_proc: ->(event) { event.approved? && event.plan.reimbursements_enabled? }
         },
         {
           name: "Card grants",
           path_proc: ->(event_id) { edit_event_path(event_id, tab: "card_grants") },
           symbol: :settings_card_grants,
-          available_proc: ->(event) { event.plan.card_grants_enabled? },
+          available_proc: ->(event) { event.approved? && event.plan.card_grants_enabled? },
           tooltip: "Manage card grants"
         },
         {
