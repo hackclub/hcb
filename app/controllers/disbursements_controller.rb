43be9c525a538367dd459db5cea2a4569b70b427
Manu Gurudath <manusvathgurudath@gmail.com>
2026-06-15 23:42:05 -0700

    Update disbursements_controller.rb

--- app/controllers/disbursements_controller.rb
+++ app/controllers/disbursements_controller.rb
@@ -61,10 +61,10 @@ class DisbursementsController < ApplicationController
     @source_event = Event.friendly.find_by_public_id(params[:source_event_id]) if params[:source_event_id]
 
     base = if admin_signed_in?
-             Event.select(:name, :id, :demo_mode, :slug, :can_front_balance, :financially_frozen).limit(10).reorder(Event::CUSTOM_SORT).includes(:plan)
+             Event.select(:name, :id, :slug, :can_front_balance, :financially_frozen).limit(10).reorder(Event::CUSTOM_SORT).includes(:plan)
            elsif !sending && @source_event&.plan&.unrestricted_disbursements_enabled?
              allowed_destination_event_ids = current_user.manageable_events.not_hidden.filter_demo_mode(false).select(:id) + Event.indexable.select(:id)
-             Event.where(id: allowed_destination_event_ids).select(:name, :id, :demo_mode, :can_front_balance, :slug, :financially_frozen).includes(:plan)
+             Event.where(id: allowed_destination_event_ids).select(:name, :id, :can_front_balance, :slug, :financially_frozen).includes(:plan)
            else
              current_user.manageable_events.not_hidden.filter_demo_mode(false).limit(10)
            end
@@ -84,7 +84,6 @@ class DisbursementsController < ApplicationController
     options = events.map do |e|
       disabled_message = "Insufficient balance" if sending && !admin_signed_in? && e.balance_available <= 0
       disabled_message = "Demo organization" if e.demo_mode && disabled_message.nil?
-      disabled_message = "Frozen organization" if e.financially_frozen? && disabled_message.nil?
       disabled_message = "HCB transfers disabled" if sending && !policy(e).create_transfer?
 
 
