c989feb7859cc9bb1213224a6c45f6627ffadd85
Manu Gurudath <manusvathgurudath@gmail.com>
2026-06-20 22:38:54 -0700

    Support data-turbo-confirm-danger for confirms
    Change Turbo.config.forms.confirm to accept (message, formElement,
    submitter) and detect data-turbo-confirm-danger on the submitter or form
    to
    enable dangerMode. Add turbo_confirm_danger: true to dangerous
    delete/reject
    /archive actions throughout the views

--- app/javascript/confirm.js
+++ app/javascript/confirm.js
@@ -13,8 +13,13 @@ function showConfirm(
   }).then(v => !!v)
 }
 
-Turbo.config.forms.confirm = message =>
-  showConfirm(message, { dangerMode: true })
+Turbo.config.forms.confirm = (message, formElement, submitter) => {
+  const dangerMode = Boolean(
+    submitter?.hasAttribute('data-turbo-confirm-danger') ||
+      formElement?.hasAttribute('data-turbo-confirm-danger')
+  )
+  return showConfirm(message, { dangerMode })
+}
 window.showConfirm = showConfirm
 window.swal = swal
 
