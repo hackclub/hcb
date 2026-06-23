ed99087c688c0dd139dfbc84a749379a51909322
Samuel Fernandez <me@sfernandez.dev>
2026-06-23 13:28:17 -0400

    Delete extra migrations

--- db/migrate/20260623134702_add_foreign_key_to_user_legal_entity.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToUserLegalEntity < ActiveRecord::Migration[8.0]
-  def change
-    add_foreign_key :users, :legal_entities, validate: false
-  end
-end
--- db/migrate/20260623135035_validate_add_foreign_key_to_user_legal_entity.rb
+++
@@ -1,5 +0,0 @@
-class ValidateAddForeignKeyToUserLegalEntity < ActiveRecord::Migration[8.0]
-  def change
-    validate_foreign_key :users, :legal_entities
-  end
-end
