3eda2e6524cd275f4ba29f185dfda7321b4798a2
Gary Tou <gary@garytou.com>
2026-06-24 11:47:35 -0700

    Apply suggestion from @garyhtou

--- spec/services/stripe_cardholder_service/create_spec.rb
+++ spec/services/stripe_cardholder_service/create_spec.rb
@@ -14,15 +14,5 @@ RSpec.describe StripeCardholderService::Create do
 
       expect { service.run }.to raise_error(ArgumentError, /phone number must be verified/)
     end
-
-    it "raises when phone number is verified but event is unapproved" do
-      user = create(:user, phone_number: "+18556254225", phone_number_verified: true)
-      unapproved_event = create(:event)
-      unapproved_event.update_column(:aasm_state, "unapproved")
-
-      service = described_class.new(current_user: user, ip_address:, event_id: unapproved_event.id)
-
-      expect { service.run }.to raise_error(ArgumentError, /not permitted under spend only plan/)
-    end
   end
 end
