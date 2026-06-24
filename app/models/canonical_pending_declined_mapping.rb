066098bc99203d564d225a5b5c8a0c6f1a3343ee
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-06-24 11:40:13 -0400

    Add ledger item amount update callback to canonical pending mappings

--- app/models/canonical_pending_declined_mapping.rb
+++ app/models/canonical_pending_declined_mapping.rb
@@ -20,4 +20,8 @@
 class CanonicalPendingDeclinedMapping < ApplicationRecord
   belongs_to :canonical_pending_transaction
 
+  after_commit if: -> { canonical_pending_transaction.ledger_item.present? } do
+    ledger_item.map!
+    ledger_item.write_amount_cents!
+  end
 end
--- app/models/canonical_pending_settled_mapping.rb
+++ app/models/canonical_pending_settled_mapping.rb
@@ -40,4 +40,9 @@ class CanonicalPendingSettledMapping < ApplicationRecord
     canonical_transaction.update(ledger_item: canonical_pending_transaction.ledger_item)
   end
 
+  after_commit if: -> { canonical_pending_transaction.ledger_item.present? } do
+    ledger_item.map!
+    ledger_item.write_amount_cents!
+  end
+
 end
