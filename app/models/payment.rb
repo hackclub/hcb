50cb642448f88e932f158733b67b1e26d5c09400
Samuel Fernandez <me@sfernandez.dev>
2026-06-25 10:28:05 -0400

    Add logic to create transfers

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -103,7 +103,115 @@ class Payment < ApplicationRecord
   private
 
   def create_transfer!
-    # actually send
+    payout_method = payee.legal_entity.default_payout_method
+    case payout_method
+    when User::PayoutMethod::Check
+      safely do
+        check = event.increase_checks.build(
+          memo: "Payment for \"#{purpose}\"."[0...40],
+          amount: amount_cents,
+          payment_for: "Payment for \"#{purpose}\".",
+          recipient_name: payee.preferred_name,
+          address_line1: payout_method.address_line1,
+          address_line2: payout_method.address_line2,
+          address_city: payout_method.address_city,
+          address_state: payout_method.address_state,
+          address_zip: payout_method.address_postal_code,
+          user: User.system_user
+        )
+
+        check.save!
+
+        self.payout = check
+        save!
+
+        transfer_receipts(check.local_hcb_code)
+      end
+    when User::PayoutMethod::AchTransfer
+      safely do
+        ach_transfer = event.ach_transfers.build(
+          amount: amount_cents,
+          payment_for: "Payment for \"#{purpose}\".",
+          recipient_name: payee.preferred_name,
+          routing_number: payout_method.routing_number,
+          account_number: payout_method.account_number,
+          bank_name: (ColumnService.get("/institutions/#{payout_method.routing_number}")["full_name"] rescue "Bank Account"),
+          creator: User.system_user
+        )
+
+        ach_transfer.save!
+
+        self.payout = ach_transfer
+        save!
+
+        transfer_receipts(ach_transfer.local_hcb_code)
+      end
+    when User::PayoutMethod::Wire
+      safely do
+        wire = event.wires.build(
+          memo: "Payment for \"#{purpose}\".",
+          payment_for: "Payment for #{purpose}."[0...140],
+          amount_cents:,
+          address_line1: payout_method.address_line1,
+          address_line2: payout_method.address_line2,
+          address_city: payout_method.address_city,
+          address_state: payout_method.address_state,
+          address_postal_code: payout_method.address_postal_code,
+          recipient_country: payout_method.recipient_country,
+          recipient_name: payout_method.recipient_name.presence || payee.preferred_name,
+          account_number: payout_method.account_number,
+          bic_code: payout_method.bic_code,
+          recipient_information: payout_method.recipient_information.merge({
+                                                                             purpose_code: Wire.payment_purpose_code_for(payout_method.recipient_country),
+                                                                             remittance_info: Wire.payment_remittance_info_for(payout_method.recipient_country),
+                                                                           }),
+          currency: "USD",
+          user: User.system_user
+        )
+
+        wire.save!
+
+        self.payout = wire
+        save!
+
+        transfer_receipts(wire.local_hcb_code)
+      end
+    when User::PayoutMethod::WiseTransfer
+      safely do
+        wise = event.wise_transfers.build(
+          memo: "Payment for \"#{purpose}\"",
+          amount: amount_cents,
+          payment_for: "Payment for \"#{purpose}\"",
+          recipient_name: payee.preferred_name,
+          recipient_email: payee.email,
+          address_line1: payout_method.address_line1,
+          address_line2: payout_method.address_line2,
+          address_city: payout_method.address_city,
+          address_state: payout_method.address_state,
+          address_postal_code: payout_method.address_postal_code,
+          recipient_country: payout_method.recipient_country,
+          bank_name: payout_method.bank_name,
+          recipient_information: payout_method.recipient_information,
+          currency:,
+          user: User.system_user
+        )
+
+        wise.save!
+
+        self.payout = wise
+        save!
+
+        transfer_receipts(wise.local_hcb_code)
+      end
+    else
+      raise ArgumentError, "🚨⚠️ unsupported payout method!"
+    end
+  end
+
+  def transfer_receipts(hcb_code)
+    receipts.each do |receipt|
+      receipt.update!(receiptable: hcb_code)
+    end
   end
 
 end
