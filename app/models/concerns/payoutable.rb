fa0ec6fa0f2bd2cd98a056cff83e92520b6f1a34
Samuel Fernandez <me@sfernandez.dev>
2026-06-29 13:06:34 -0400

    Update payoutable validation

--- app/models/concerns/payoutable.rb
+++ app/models/concerns/payoutable.rb
@@ -4,8 +4,9 @@ module Payoutable
   extend ActiveSupport::Concern
   included do
     validate do
-      if reimbursement_payout_holding.present? && employee_payment.present?
-        errors.add(:base, "A transfer can not belong to both a reimbursement payout holding and an employee payment.")
+      associations = [reimbursement_payout_holding, employee_payment, payment_attempt].count(&:present?)
+      if associations > 1
+        errors.add(:base, "A transfer can not belong to more than one of: reimbursement payout holding, employee payment, or payment attempt.")
       end
     end
   end
