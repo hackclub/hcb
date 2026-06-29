9e17f545aa9f45b706c41b2ec084a89929191908
Manu Gurudath <manusvathgurudath@gmail.com>
2026-06-29 15:03:38 -0400

    lint again

--- app/controllers/events/payees_controller.rb
+++ app/controllers/events/payees_controller.rb
@@ -1,26 +1,28 @@
 # frozen_string_literal: true
 
-class Events::PayeesController < ApplicationController
-  include SetEvent
+module Events
+  class PayeesController < ApplicationController
+    include SetEvent
 
-  before_action :set_event
+    before_action :set_event
 
-  def index
-    authorize @event.payees.build, :index?
-    @payees = params[:q].present? ? @event.payees.search(params[:q]) : @event.payees
-    render layout: false
-  end
-
-  def create
-    payee = @event.payees.build(display_name: params[:name], email: params[:email])
-    authorize payee
-
-    if payee.save
-      redirect_to event_payments_new_path(event_id: @event.slug, payee_id: payee.id)
-    else
-      redirect_to event_payments_new_path(event_id: @event.slug),
-                  alert: payee.errors.full_messages.to_sentence
+    def index
+      authorize @event.payees.build, :index?
+      @payees = params[:q].present? ? @event.payees.search(params[:q]) : @event.payees
+      render layout: false
     end
-  end
 
+    def create
+      payee = @event.payees.build(display_name: params[:name], email: params[:email])
+      authorize payee
+
+      if payee.save
+        redirect_to event_payments_new_path(event_id: @event.slug, payee_id: payee.id)
+      else
+        redirect_to event_payments_new_path(event_id: @event.slug),
+                    alert: payee.errors.full_messages.to_sentence
+      end
+    end
+
+  end
 end
