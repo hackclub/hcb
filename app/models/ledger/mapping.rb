561f263967df0db1a6e09ef76c97ecea129a2357
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-06-30 17:07:22 -0400

    Update `after_commit` hook

--- app/models/ledger/mapping.rb
+++ app/models/ledger/mapping.rb
@@ -42,8 +42,7 @@ class Ledger
     validates :ledger_item_id, uniqueness: { conditions: -> { where(on_primary_ledger: true) }, message: "is already mapped on a primary ledger" }, if: :on_primary_ledger?
     validate :on_primary_ledger_matches_ledger_primary
 
-    after_commit if: -> { ledger_item.present? } do
-      ledger_item.map!
+    after_commit do
       ledger_item.write_amount_cents!
     end
 
