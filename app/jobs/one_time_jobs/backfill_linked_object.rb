981fb1616251f3e5180f74e4635edb26ac051823
yodalightsabr <yoda@yodacode.xyz>
2026-06-30 19:31:34 -0700

    Apply suggestion from @YodaLightsabr

--- app/jobs/one_time_jobs/backfill_linked_object.rb
+++ app/jobs/one_time_jobs/backfill_linked_object.rb
@@ -6,8 +6,8 @@ module OneTimeJobs
 
     def perform(event_id: nil)
       event = Event.find(event_id)
-
       ledger_items = event.ledger_items
+
       ledger_items.find_each do |li|
         li.update!(linked_object: li.hcb_code.linked_object) unless li.hcb_code.linked_object.nil?
       end
