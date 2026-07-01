bed5a512da8ea87d3cd3bb3000c66cbd74cd21dd
yodalightsabr <yoda@yodacode.xyz>
2026-06-30 19:05:51 -0700

    Apply suggestion from @YodaLightsabr

--- db/migrate/20260626195451_add_linked_object_to_ledger_items.rb
+++ db/migrate/20260626195451_add_linked_object_to_ledger_items.rb
@@ -2,6 +2,6 @@ class AddLinkedObjectToLedgerItems < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_reference :ledger_items, :linked_object, polymorphic: true, null: true, index: {algorithm: :concurrently}
+    add_reference :ledger_items, :linked_object, polymorphic: true, null: true, index: { algorithm: :concurrently }
   end
 end
