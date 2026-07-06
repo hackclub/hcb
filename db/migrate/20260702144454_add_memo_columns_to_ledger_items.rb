0b2341543c4a2447a732de453af735a3e00fb5cb
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-06 19:49:41 +0000

    update migration

--- db/migrate/20260702144454_add_memo_columns_to_ledger_items.rb
+++ db/migrate/20260702144454_add_memo_columns_to_ledger_items.rb
@@ -2,6 +2,5 @@ class AddMemoColumnsToLedgerItems < ActiveRecord::Migration[8.0]
   def change
     add_column :ledger_items, :system_memo, :text
     add_column :ledger_items, :custom_memo, :text
-    add_column :ledger_items, :original_memo, :text
   end
 end
