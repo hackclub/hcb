0e537a19edcefcd242a9b90baf9faa8817067c02
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-06 15:16:55 +0000

    delete decorator

--- app/decorators/linked_object_decorator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class LinkedObjectDecorator < SimpleDelegator
-  def author
-    # This method should be overwritten in specific classes
-    raise NotImplementedError, "The #{self.class.name} model inherits from LinkedObjectDecorator, but hasn't implemented it's own version of author."
-  end
-
-end
