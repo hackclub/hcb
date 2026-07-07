dcf1cf1dd462c80f981686911c85c207b33459a1
Manu Gurudath <manusvathgurudath@gmail.com>
2026-07-07 13:53:21 -0400

    Update payout_toggle_controller.js

--- app/javascript/controllers/payout_toggle_controller.js
+++ app/javascript/controllers/payout_toggle_controller.js
@@ -1,10 +1,5 @@
 import { Controller } from '@hotwired/stimulus'
 
-<<<<<<< HEAD
-=======
-// Toggles the manual-only sections (Payout settings, Tax information) and their
-// sidebar steps based on whether a manual payout is selected in the payment details.
->>>>>>> c9dad95daa63a366da99b3a8408f44f3504a6a1c
 export default class extends Controller {
   static targets = ['section']
 
