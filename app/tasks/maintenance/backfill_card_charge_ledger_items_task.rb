47b44c99593c962a2c07ac2d3cbea3b5b434f2f5
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-07-09 13:28:10 -0400

    warn of anomalies

--- app/tasks/maintenance/backfill_card_charge_ledger_items_task.rb
+++ app/tasks/maintenance/backfill_card_charge_ledger_items_task.rb
@@ -14,7 +14,11 @@ module Maintenance
     def process(card_charge)
       ledger_item = card_charge.raw_pending_stripe_transaction&.canonical_pending_transaction&.ledger_item
       ledger_item ||= card_charge.raw_stripe_transactions.filter_map { |rst| rst.canonical_transaction&.ledger_item }.first
-      return if ledger_item.nil?
+        
+      if ledger_item.nil?
+        Rails.error.report AnomalyError.new("CardCharge #{card_charge.id} has no CT or CPT with a ledger item")
+        return
+      end
 
       if ledger_item.linked_object.present?
         unless ledger_item.linked_object == card_charge
