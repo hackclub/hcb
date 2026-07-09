040ac985ef4700cf3d081c39806fdc7d40193700
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-09 21:12:43 +0000

    [CPDM] Destroy CPSMs when created

--- app/models/canonical_pending_declined_mapping.rb
+++ app/models/canonical_pending_declined_mapping.rb
@@ -20,6 +20,17 @@
 class CanonicalPendingDeclinedMapping < ApplicationRecord
   belongs_to :canonical_pending_transaction
 
+  after_create_commit do
+    # Sometimes a reimbursement report will be reversed after the money has
+    # moved from the organization to HCB Reimbursement Clearinghouse. Upon reversal,
+    # HCB creates a CanonicalPendingDeclinedMapping for the CanonicalPendingTransaction.
+    # So, we need to destroy the settle mapping if it's going to be declined.
+    if canonical_pending_transaction.canonical_pending_settled_mappings.any?
+      canonical_pending_transaction.canonical_pending_settled_mappings.destroy_all
+      Rails.error.unexpected "CPT ##{canonical_pending_transaction.id} had both a decline and a settle mapping. The settle mapping was destroyed."
+    end
+  end
+
   after_commit if: -> { canonical_pending_transaction.ledger_item.present? } do
     canonical_pending_transaction.ledger_item.map!
     canonical_pending_transaction.ledger_item.refresh!
