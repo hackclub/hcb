7d04af2e3384bb2a8a01f4ff22e3bd138650e816
Samuel Fernandez <me@sfernandez.dev>
2026-07-13 18:27:46 -0400

    Prevent admins from taking LE actions for other users

--- app/policies/tax/form_policy.rb
+++ app/policies/tax/form_policy.rb
@@ -3,7 +3,7 @@
 module Tax
   class FormPolicy < ApplicationPolicy
     def show?
-      user.auditor? || record.legal_entity.users.include?(user)
+      user.auditor? || user_in_legal_entity?
     end
 
     def create?
@@ -11,12 +11,26 @@ module Tax
     end
 
     def sync?
-      user.admin? || record.legal_entity.users.include?(user)
+      user.admin? || user_in_legal_entity?
     end
 
-    alias_method :switch_legal_entity?, :sync?
-    alias_method :create_legal_entity?, :sync?
-    alias_method :discard?, :sync?
+    def create_legal_entity?
+      user_in_legal_entity?
+    end
+
+    def switch_legal_entity?
+      user_in_legal_entity?
+    end
+
+    def discard?
+      user_in_legal_entity?
+    end
+
+    private
+
+    def user_in_legal_entity?
+      record.legal_entity.users.include?(user)
+    end
 
   end
 end
