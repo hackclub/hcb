07efc5955d8d835578b342f95c1589692bf64c28
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-16 19:23:10 +0000

    Update missing receipts list

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -70,7 +70,7 @@ class MyController < ApplicationController
   end
 
   def missing_receipts_list
-    @missing = current_user.transactions_missing_receipt
+    @missing = Flipper.enabled?(:new_ledger_everywhere_2026_07_13, current_user) ? current_user.ledger_items_missing_receipt : current_user.transactions_missing_receipt
 
     if @missing.any?
       render :missing_receipts_list, layout: !request.xhr?
