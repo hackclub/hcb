1fa3d05bda02af047c09f2a3fada56edd444908e
Manu Gurudath <manusvathgurudath@gmail.com>
2026-07-29 12:27:41 -0700

    Update onboarding_reminder_job.rb

--- app/jobs/payroll/position/onboarding_reminder_job.rb
+++ app/jobs/payroll/position/onboarding_reminder_job.rb
@@ -7,11 +7,8 @@ module Payroll
       discard_on ActiveJob::DeserializationError
 
       def perform(position)
-        # Managed contractors have their onboarding handled by the org, so they
-        # don't get these nudges.
         return if position.payee.managed?
 
-        # Stop once they've finished (or the position is no longer onboarding).
         return unless position.onboarding? && position.contractor_onboarding_incomplete?
 
         Payroll::PositionMailer.with(position:).onboarding_reminder.deliver_later
