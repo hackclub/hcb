d61600eb91de614e6b094258d0e4d12002bf4d8c
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-31 21:04:15 +0000

    fix expense payouts

--- app/models/ledger/item.rb
+++ app/models/ledger/item.rb
@@ -167,7 +167,8 @@ class Ledger
     end
 
     def calculate_status
-      return :settled if linked_object_type.in?(["Reimbursement::ExpensePayout", "BankFee"])
+      return :settled if linked_object_type == "BankFee"
+      return :settled if linked_object_type == "Reimbursement::ExpensePayout" && canonical_pending_transactions.exists? && canonical_transactions.none?
       return :settled if linked_object_type == "Disbursement::Outgoing" && linked_object.counterparty.canonical_pending_transactions.fronted.not_declined.any?
       return :settled if linked_object_type.in?(["Disbursement::Outgoing", "Disbursement::Incoming"]) && linked_object.approved_at.present? && !linked_object.rejected? && !linked_object.errored?
       return :settled if canonical_pending_transactions.fronted.not_declined.revenue.any? && primary_ledger&.can_front_balance?
