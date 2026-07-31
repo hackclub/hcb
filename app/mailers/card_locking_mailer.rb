952d11cff50807af4568741cb24a0a574a29e2ba
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-31 20:30:41 +0000

    remove dead code

--- app/mailers/card_locking_mailer.rb
+++ app/mailers/card_locking_mailer.rb
@@ -30,10 +30,4 @@ class CardLockingMailer < ApplicationMailer
     @delivery_reason = "you spent funds with an HCB Visa® Commercial card and are required to upload receipts for all funds spent. #{stripe_issuing_disclosure}"
   end
 
-  def set_transaction_data
-    @hcb_codes = @user.transactions_missing_receipt(from: Receipt::CARD_LOCKING_START_DATE, to: 24.hours.ago)
-    @hcb_codes_upcoming = @user.transactions_missing_receipt(from: 24.hours.ago)
-    @show_org = @user.events.size > 1
-  end
-
 end
