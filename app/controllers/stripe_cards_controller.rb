01bddfda1379acdc0bc1e9911934404aadc44bd6
Manu Gurudath <manusvathgurudath@gmail.com>
2026-08-04 10:57:43 -0700

    Update stripe_cards_controller.rb

--- app/controllers/stripe_cards_controller.rb
+++ app/controllers/stripe_cards_controller.rb
@@ -77,8 +77,6 @@ class StripeCardsController < ApplicationController
     end
 
     @show_card_details = params[:show_details] == "true"
-    # `Event` is paranoid, so `@card.event` is nil when the org has been deleted.
-    # Only auditors can reach that page (`role_at_least?` is false without an event).
     @event = @card.event || Event.with_deleted.find_by(id: @card.event_id)
 
     @hcb_codes = @card.local_hcb_codes
