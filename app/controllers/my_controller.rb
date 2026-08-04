d6e014937e625f54b5fe5611a1618d1b43ace74a
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-08-04 15:51:35 +0000

    use ternary

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -70,11 +70,7 @@ class MyController < ApplicationController
   end
 
   def missing_receipts_list
-    @missing = if Flipper.enabled?(:new_ledger_everywhere_2026_07_13, current_user)
-                 current_user.ledger_items_missing_receipt.page(params[:page]).per((params[:per] || 100).to_i.clamp(1, 200))
-               else
-                 current_user.transactions_missing_receipt
-               end
+    @missing = Flipper.enabled?(:new_ledger_everywhere_2026_07_13, current_user) ? current_user.ledger_items_missing_receipt.page(params[:page]).per((params[:per] || 100).to_i.clamp(1, 200)) : current_user.transactions_missing_receipt
 
     if @missing.any?
       render :missing_receipts_list, layout: !request.xhr?
