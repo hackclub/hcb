c7a65007b109d5ab0ac18d946b9f85a486b0b3bc
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-08-05 18:19:03 +0000

    [Schema] Fix CPSM unique migration

--- db/migrate/20260804120000_add_unique_indexes_to_canonical_pending_settled_mappings.rb
+++ db/migrate/20260804120000_add_unique_indexes_to_canonical_pending_settled_mappings.rb
@@ -9,11 +9,5 @@ class AddUniqueIndexesToCanonicalPendingSettledMappings < ActiveRecord::Migratio
     add_index :canonical_pending_settled_mappings, :canonical_pending_transaction_id,
               unique: true, algorithm: :concurrently,
               name: "index_cpsm_on_cpt_id"
-
-    remove_index :canonical_pending_settled_mappings, :canonical_transaction_id
-
-    add_index :canonical_pending_settled_mappings, :canonical_transaction_id,
-              unique: true, algorithm: :concurrently,
-              name: "index_cpsm_on_ct_id"
   end
 end
