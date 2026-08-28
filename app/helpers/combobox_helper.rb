5ce012c8faca1fbe51e5c9ee42a6d710876f56c8
Manu Gurudath <manusvathgurudath@gmail.com>
2026-08-28 15:12:12 -0700

    Update combobox_helper.rb

--- app/helpers/combobox_helper.rb
+++ app/helpers/combobox_helper.rb
@@ -2,41 +2,29 @@
 
 module ComboboxHelper
   DEFAULT_COMBOBOX_PLACEHOLDER = "Select one…"
-
-  COMBOBOX_INPUT_ACTIONS = %w[
-    input->combobox#onInput
-    focus->combobox#onFocus
-    keydown->combobox#onKeydown
-    blur->combobox#onBlur
-  ].join(" ").freeze
+  COMBOBOX_INPUT_ACTIONS = "input->combobox#onInput focus->combobox#onFocus " \
+                           "keydown->combobox#onKeydown blur->combobox#onBlur"
 
   def combobox_tag(name, src, selected: nil, wrapper_class: nil, data: {}, **input_options)
     value, display = combobox_selection(selected)
-    input_id = input_options.delete(:id) || combobox_id_for(name)
+    input_id = input_options.delete(:id) || name.to_s.gsub(/\W+/, "_").delete_suffix("_")
     listbox_id = "#{input_id}_listbox"
 
-    field = tag.div class: "combobox__field" do
-      tag.input(**{
-        type: "text",
-        id: input_id,
-        role: "combobox",
-        class: token_list("combobox__input", input_options.delete(:class)),
-        placeholder: DEFAULT_COMBOBOX_PLACEHOLDER,
-        autocomplete: "off",
-        aria: { autocomplete: "both", expanded: false, controls: listbox_id },
-        data: { combobox_target: "input", action: COMBOBOX_INPUT_ACTIONS }
-      }.deep_merge(input_options)) + tag.div(class: "combobox__handle")
-    end
+    input = tag.input(**{
+      type: "text", id: input_id, role: "combobox",
+      class: token_list("combobox__input", input_options.delete(:class)),
+      placeholder: DEFAULT_COMBOBOX_PLACEHOLDER, autocomplete: "off",
+      aria: { autocomplete: "both", expanded: false, controls: listbox_id },
+      data: { combobox_target: "input", action: COMBOBOX_INPUT_ACTIONS }
+    }.deep_merge(input_options))
 
     tag.div class: token_list("combobox", wrapper_class),
-            data: {
-              controller: token_list("combobox", data[:controller]),
-              combobox_url_value: src,
-              combobox_selected_value: value,
-              combobox_label_value: display
-            }.merge(data.except(:controller)) do
+            data: { controller: token_list("combobox", data[:controller]),
+                    combobox_url_value: src, combobox_selected_value: value,
+                    combobox_label_value: display
+}.merge(data.except(:controller)) do
       safe_join [
-        field,
+        tag.div(input + tag.div(class: "combobox__handle"), class: "combobox__field"),
         hidden_field_tag(name, value, id: nil, data: { combobox_target: "hidden" }),
         tag.ul(nil, role: "listbox", id: listbox_id, class: "combobox__listbox",
                     hidden: true, data: { combobox_target: "listbox" })
@@ -48,13 +36,9 @@ module ComboboxHelper
 
   def combobox_selection(selected)
     case selected
-    when nil then [nil, nil]
-    when Hash then [selected[:value], selected[:label]]
+    when nil then []
+    when Hash then selected.values_at(:value, :label)
     else [selected.id, selected.to_combobox_display]
     end
-  end
-
-  def combobox_id_for(name)
-    name.to_s.gsub(/\W+/, "_").delete_suffix("_")
   end
 end
