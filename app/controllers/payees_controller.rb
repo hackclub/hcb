cbfdea24693fdfdd7c21e65636a581f9630ae9ed
Manu Gurudath <manusvathgurudath@gmail.com>
2026-08-28 14:05:13 -0700

    Update payees_controller.rb

--- app/controllers/payees_controller.rb
+++ app/controllers/payees_controller.rb
@@ -8,16 +8,12 @@ class PayeesController < ApplicationController
 
   class InvalidManualPayeeEntityType < StandardError; end
 
-  PER_PAGE = 15
-
   def index
     authorize @event
     all = @event.payees.not_archived.includes(:legal_entity, :payments)
     payees = params[:q].present? ? all.search(params[:q]) : all
-    @payees = payees.order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+    @payees = payees.order(created_at: :desc).page(params[:page]).per(15)
 
-    # Pin the selected recipient to the top of the list, unless it's already on
-    # the page being displayed.
     @selected = all.find_by_hashid(params[:payee_id]) if params[:payee_id].present?
     @selected = nil if @selected && @payees.include?(@selected)
 
