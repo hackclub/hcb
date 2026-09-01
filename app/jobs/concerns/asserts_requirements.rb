b6393e44b94a3d2c17cdaa76e6798b3dfde72ee9
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-09-01 11:14:00 -0700

    Create mailer for failed assertion jobs

--- app/jobs/concerns/asserts_requirements.rb
+++ app/jobs/concerns/asserts_requirements.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 module AssertsRequirements
+  class FailedAssertionError < StandardError; end
+  class FailedJobError < StandardError; end
+
   extend ActiveSupport::Concern
 
   included do
-    class FailedAssertionError < StandardError; end
-
-    class FailedJobError < StandardError; end
-
     def report_anomaly(message)
       @anomalies << message
-      Rails.error.report(FailedAssertionError.new(message))
+      Rails.error.report(AssertsRequirements::FailedAssertionError.new(message))
     end
 
     def perform
@@ -19,7 +18,8 @@ module AssertsRequirements
       run
 
       if @anomalies.any?
-        Rails.error.report(FailedJobError.new("#{self.class.name} failed with #{@anomalies.count} anomalies"))
+        Rails.error.report(AssertsRequirements::FailedJobError.new("#{self.class.name} failed with #{@anomalies.count} anomalies (#{job_id})"))
+        AdminMailer.failed_assertion_job(job: self.class.name, job_id:, anomalies: @anomalies).deliver_now
       end
 
       @anomalies
