d694cae99da6ea07f40aa121802f3535e3041e52
Manu Gurudath <manusvathgurudath@gmail.com>
2026-09-01 09:31:33 -0700

    Improve form UI

--- config/routes.rb
+++ config/routes.rb
@@ -1132,7 +1132,6 @@ Rails.application.routes.draw do
         post "disable_pre_authorization"
         post "permit_merchant"
 
-        get "edit/overview", to: "card_grants#edit_overview"
         get "edit/usage_restrictions", to: "card_grants#edit_usage_restrictions"
         get "edit/expiration", to: "card_grants#edit_expiration"
         get "edit/purpose", to: "card_grants#edit_purpose"
