70c3e8f6327b313763595edb8c82f9ad599cc113
buffalo09 <166964716+eyedecay@users.noreply.github.com>
2026-09-03 14:33:26 +0000

    [RSpec] Add Tests for window expiry

--- spec/services/login_code_service/request_spec.rb
+++ spec/services/login_code_service/request_spec.rb
@@ -120,5 +120,43 @@ describe LoginCodeService::Request do
       response = described_class.new(email: user.email, sms: true, ip_address:, user_agent:).run
       expect(response[:error]).to eq("You're requesting too many SMS codes. Please try again later.")
     end
+    it "allows email codes again after the rate limit window expires" do
+      user = create(:user)
+      allow(LoginCodeMailer).to receive(:send_code).and_return(double(deliver_now: true))
+
+      20.times { described_class.new(email: user.email, ip_address:, user_agent:).run }
+
+      response = described_class.new(email: user.email, ip_address:, user_agent:).run
+      expect(response[:error]).to be_present
+
+      travel(1.hour)
+      Rails.cache.clear
+
+      allow(LoginCodeMailer).to receive(:send_code).and_return(double(deliver_now: true))
+      response = described_class.new(email: user.email, ip_address:, user_agent:).run
+      expect(response[:error]).to be_nil
+      expect(response[:method]).to eq(:email)
+    end
+
+    it "allows sms codes again after the rate limit window expires" do
+      user = create(:user)
+      user.update!(phone_number: "+18556254225", use_sms_auth: true, phone_number_verified: true)
+      stub_twilio_sms_verification(phone_number: user.phone_number, code: "123456")
+
+      20.times do
+        described_class.new(email: user.email, sms: true, ip_address:, user_agent:).run
+      end
+
+      response = described_class.new(email: user.email, sms: true, ip_address:, user_agent:).run
+      expect(response[:error]).to be_present
+
+      travel(1.hour)
+      Rails.cache.clear
+
+      stub_twilio_sms_verification(phone_number: user.phone_number, code: "123456")
+      response = described_class.new(email: user.email, sms: true, ip_address:, user_agent:).run
+      expect(response[:error]).to be_nil
+      expect(response[:method]).to eq(:sms)
+    end
   end
 end
