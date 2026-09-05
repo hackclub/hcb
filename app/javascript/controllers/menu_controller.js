51fd2c53481a12e7a1fecfbc62a21587ba4ff6a7
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-09-05 18:20:15 -0400

    Revert "[Admin] Close pagination per-page menu after a size is selected (#14899)"
    This reverts commit 27ae6ead25ba9558c4df5fc93f3ee65f0410474c.

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -30,11 +30,6 @@ export default class extends Controller {
 
   disconnect() {
     this.cleanup && this.cleanup()
-    // remove cloned popup so it doesn't outlive turbo frame updates.
-    this.content && this.content.remove()
-    this.content = undefined
-    this.cleanup = undefined
-    this.isOpen = false
     document.removeEventListener('hcb:close-menus', this._closeMenusHandler)
   }
 
