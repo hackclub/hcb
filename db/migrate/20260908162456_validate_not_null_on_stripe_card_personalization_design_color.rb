b679e0b8b8c9c1aa3362edaefc2c219a49df8bf4
24c02 <163450896+24c02@users.noreply.github.com>
2026-09-08 19:04:17 -0400

    re-add check constraint in rollback so migration 1 can drop it

--- db/migrate/20260908162456_validate_not_null_on_stripe_card_personalization_design_color.rb
+++ db/migrate/20260908162456_validate_not_null_on_stripe_card_personalization_design_color.rb
@@ -7,5 +7,6 @@ class ValidateNotNullOnStripeCardPersonalizationDesignColor < ActiveRecord::Migr
 
   def down
     change_column_null :stripe_card_personalization_designs, :color, true
+    add_check_constraint :stripe_card_personalization_designs, "color IS NOT NULL", name: "stripe_card_personalization_designs_color_null", validate: false
   end
 end
