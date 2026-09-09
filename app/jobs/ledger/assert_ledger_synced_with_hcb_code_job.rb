a6204cc9ad2703a6816f090c8cddffa26c335277
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-09-09 18:39:23 -0400

    Update app/jobs/ledger/assert_ledger_synced_with_hcb_code_job.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/jobs/ledger/assert_ledger_synced_with_hcb_code_job.rb
+++ app/jobs/ledger/assert_ledger_synced_with_hcb_code_job.rb
@@ -4,11 +4,11 @@ class Ledger
   class AssertLedgerSyncedWithHcbCodeJob < AssertRequirementJob
     def perform
       @ledger_items = Ledger::Item.where(id: Ledger::Mapping
-        .left_joins(ledger: [:event, { card_grant: :subledger }], ledger_item: :hcb_code)
-        .joins("LEFT JOIN events hcb_events ON hcb_events.id = hcb_codes.event_id")
-        .joins("LEFT JOIN card_grants hcb_subledgers ON hcb_subledgers.id = hcb_codes.subledger_id")
-        .where("events.id != hcb_events.id OR subledgers.id != hcb_subledgers.id")
-        .select("ledger_mappings.ledger_item_id as ledger_item_id"))
+                                             .left_joins(ledger: [:event, { card_grant: :subledger }], ledger_item: :hcb_code)
+                                             .joins("LEFT JOIN events hcb_events ON hcb_events.id = hcb_codes.event_id")
+                                             .joins("LEFT JOIN card_grants hcb_subledgers ON hcb_subledgers.id = hcb_codes.subledger_id")
+                                             .where("events.id != hcb_events.id OR subledgers.id != hcb_subledgers.id")
+                                             .select("ledger_mappings.ledger_item_id as ledger_item_id"))
 
       @ledger_items.find_each do |item|
         safely do
