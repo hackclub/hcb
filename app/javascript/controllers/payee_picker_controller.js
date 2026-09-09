7bc5963267132abeb0690bb22766bd093d2a3f1c
Manu Gurudath <manusvathgurudath@gmail.com>
2026-09-08 22:55:59 -0700

    Update payee_picker_controller.js

--- app/javascript/controllers/payee_picker_controller.js
+++ app/javascript/controllers/payee_picker_controller.js
@@ -17,8 +17,12 @@ export default class extends Controller {
 
   async showAddingFromSearch(event) {
     if (event.params.query) {
-      const filled = event.params.isEmail ? 'emailInputTarget' : 'nameInputTarget'
-      const other = event.params.isEmail ? 'nameInputTarget' : 'emailInputTarget'
+      const filled = event.params.isEmail
+        ? 'emailInputTarget'
+        : 'nameInputTarget'
+      const other = event.params.isEmail
+        ? 'nameInputTarget'
+        : 'emailInputTarget'
       this[filled].value = event.params.query
       this[other].value = ''
     }
