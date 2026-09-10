ca986faeb95a9f7d8256b1512e7c6cfb42582d07
Manu Gurudath <manusvathgurudath@gmail.com>
2026-09-10 13:00:57 -0700

    handle error cases

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -54,6 +54,26 @@ RSpec.describe CommentsController do
       expect(response.body).to include("src=\"#{reimbursement_report_path(report)}\"")
     end
 
+    it "streams an error flash into the popover when the comment is invalid" do
+      request.headers["Turbo-Frame"] = "reimbursement_report_#{report.id}"
+
+      expect {
+        post :create, params: {
+          reimbursement_report_id: report.id,
+          comment: {
+            content: "",
+            commentable_type: "Reimbursement::Report",
+            commentable_id: report.id,
+            return_to: frame_url
+          }
+        }
+      }.not_to change { report.comments.count }
+
+      expect(response).to have_http_status(:unprocessable_content)
+      expect(response.body).to include("shared_popover_flash")
+      expect(response.body).to include("Content can&#39;t be blank")
+    end
+
     it "redirects back to the referring page otherwise" do
       expect { create_comment }.to change { report.comments.count }.by(1)
       expect(response).to redirect_to("http://test.host/my/inbox")
