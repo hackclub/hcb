c888a4f02fa1d65342918bade647eb3b94f8f445
Matthew Soh <matthew@mattsoh.dev>
2026-09-11 09:08:28 +0100

    Apply suggestion from @Mohamad-Mortada
    Co-authored-by: Mohamad Mortada <mmortada685@gmail.com>

--- app/controllers/api/v4/disbursements_controller.rb
+++ app/controllers/api/v4/disbursements_controller.rb
@@ -9,8 +9,6 @@ module Api
 
       def show
         @disbursement = authorize Disbursement.find_by_public_id!(params[:id]), :show_in_v4?
-
-        render :show, status: :ok
       end
 
       require_oauth2_scope "transactions:read", :show
