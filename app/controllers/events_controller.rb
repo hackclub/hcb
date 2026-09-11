c132607da7bea4dcabfd686806d335b70011572a
Manu Gurudath <manusvathgurudath@gmail.com>
2026-09-10 20:53:14 -0700

    remove junk

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -930,7 +930,6 @@ class EventsController < ApplicationController
 
     can_review = Payroll::PositionPolicy.new(current_user, @event).review?
     @pending_invoice_counts = can_review ? @event.payroll_invoices.where(aasm_state: "submitted").group(:payroll_position_id).count : {}
-    @pending_invoices_count = @pending_invoice_counts.values.sum
 
     counts_by_status = @contractors.to_a.group_by(&:status).transform_values(&:count)
     @stats = {
