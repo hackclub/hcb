b69ef2b2ab5ab265fd126ba780e8bd17d0bff8fb
Samuel Fernandez <me@sfernandez.dev>
2026-09-12 16:53:17 -0400

    Add change email button

--- app/helpers/payees_helper.rb
+++ app/helpers/payees_helper.rb
@@ -8,4 +8,12 @@ module PayeesHelper
       new_event_payment_path(event_id: event.slug, **opts)
     end
   end
+
+  def updated_recipient_transfer_path(destination, event, **opts)
+    if destination.to_s == "contractors"
+      event_payroll_position_path(event_id: event.slug, **opts)
+    else
+      payment_path(event_id: event.slug, **opts)
+    end
+  end
 end
