21c7222e54739ae6274ed038aea8ab7c4bca0f10
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-09-14 11:52:07 -0700

    Bug fixes and improving performance

--- app/jobs/ledger/assert_ledger_item_memo_matches_hcb_code_job.rb
+++ app/jobs/ledger/assert_ledger_item_memo_matches_hcb_code_job.rb
@@ -3,12 +3,12 @@
 class Ledger
   class AssertLedgerItemMemoMatchesHcbCodeJob < AssertRequirementJob
     def perform
-      @ledger_items = Ledger::Item.where.associated(:hcb_code).includes(:hcb_code)
+      @ledger_items = Ledger::Item.where.associated(:hcb_code).includes(:hcb_code, hcb_code: [:canonical_transactions, :canonical_pending_transactions])
 
       @ledger_items.find_each do |item|
         safely do
-          if hcb_code.custom_memo.presence != item.custom_memo.presence
-            report_anomaly "Ledger::Item #{item.hashid} custom_memo does not match HcbCode #{hcb_code.hashid} custom_memo"
+          if item.hcb_code.custom_memo.presence != item.custom_memo.presence
+            report_anomaly "Ledger::Item #{item.hashid} custom_memo does not match HcbCode #{item.hcb_code.hashid} custom_memo"
           end
         end
       end
