b44891e908d3773f775a4d6af0bf9aeade816e82
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2026-09-14 12:45:40 -0700

    Greatly improve assertion performance

--- app/jobs/ledger/assert_cems_match_ledger_mapping_job.rb
+++ app/jobs/ledger/assert_cems_match_ledger_mapping_job.rb
@@ -3,13 +3,17 @@
 class Ledger
   class AssertCemsMatchLedgerMappingJob < AssertRequirementJob
     def perform
-      @cts = CanonicalTransaction.all.includes(:ledger_item, :canonical_event_mapping)
+      @cts = CanonicalTransaction.where(id: CanonicalEventMapping
+                                            .left_joins(:event, subledger: :card_grant, canonical_transaction: :ledger_item)
+                                            .joins("LEFT JOIN ledger_mappings primary_mappings ON primary_mappings.ledger_item_id = ledger_items.id AND primary_mappings.on_primary_ledger = true")
+                                            .joins("LEFT JOIN ledgers primary_ledgers ON primary_ledgers.id = primary_mappings.ledger_id")
+                                            .joins("LEFT JOIN ledgers cem_ledgers ON cem_ledgers.card_grant_id = card_grants.id OR (cem_ledgers.event_id = events.id AND card_grants.id IS NULL)")
+                                            .where("primary_ledgers.id != cem_ledgers.id")
+                                            .select("canonical_event_mappings.canonical_transaction_id as canonical_transaction_id"))
 
       @cts.find_each do |ct|
         safely do
-          if (cpem = ct.canonical_event_mapping) && (cpem.subledger&.card_grant || cpem.event)&.ledger != ct.ledger_item&.primary_ledger
-            report_anomaly "CanonicalTransaction #{ct.id} canonical_event_mapping does not match Ledger::Item"
-          end
+          report_anomaly "CanonicalTransaction #{ct.id} canonical_event_mapping does not match Ledger::Item"
         end
       end
     end
--- app/jobs/ledger/assert_cpems_match_ledger_mapping_job.rb
+++ app/jobs/ledger/assert_cpems_match_ledger_mapping_job.rb
@@ -3,13 +3,17 @@
 class Ledger
   class AssertCpemsMatchLedgerMappingJob < AssertRequirementJob
     def perform
-      @cpts = CanonicalPendingTransaction.all.includes(:ledger_item, :canonical_pending_event_mapping)
+      @cpts = CanonicalPendingTransaction.where(id: CanonicalPendingEventMapping
+                                                    .left_joins(:event, subledger: :card_grant, canonical_pending_transaction: :ledger_item)
+                                                    .joins("LEFT JOIN ledger_mappings primary_mappings ON primary_mappings.ledger_item_id = ledger_items.id AND primary_mappings.on_primary_ledger = true")
+                                                    .joins("LEFT JOIN ledgers primary_ledgers ON primary_ledgers.id = primary_mappings.ledger_id")
+                                                    .joins("LEFT JOIN ledgers cpem_ledgers ON cpem_ledgers.card_grant_id = card_grants.id OR (cpem_ledgers.event_id = events.id AND card_grants.id IS NULL)")
+                                                    .where("primary_ledgers.id != cpem_ledgers.id")
+                                                    .select("canonical_pending_event_mappings.canonical_pending_transaction_id as canonical_pending_transaction_id"))
 
       @cpts.find_each do |cpt|
         safely do
-          if (cpem = cpt.canonical_pending_event_mapping) && (cpem.subledger&.card_grant || cpem.event)&.ledger != cpt.ledger_item&.primary_ledger
             report_anomaly "CanonicalPendingTransaction #{cpt.id} canonical_pending_event_mapping does not match Ledger::Item"
-          end
         end
       end
     end
