e282ec4e3be13b8397eb81af77346b8e1f1fa6f6
Matthew Soh <matthew@mattsoh.dev>
2026-09-17 19:09:46 +0100

    remove backfill (because it uses API calls)

--- db/migrate/20260917140226_add_uetr_to_wires.rb
+++ db/migrate/20260917140226_add_uetr_to_wires.rb
@@ -1,29 +1,8 @@
 # frozen_string_literal: true
 
 class AddUetrToWires < ActiveRecord::Migration[8.1]
-  disable_ddl_transaction!
-
-  class MigrationWire < ActiveRecord::Base
-    self.table_name = "wires"
-
-  end
-
-  def up
+  def change
     add_column :wires, :uetr, :string
-
-    MigrationWire.reset_column_information
-
-    MigrationWire.where.not(column_id: nil).where(uetr: nil).find_each do |wire|
-      uetr = ColumnService.international_wire(wire.column_id)["uetr"]
-
-      wire.update_column(:uetr, uetr) if uetr.present?
-    rescue Faraday::Error => e
-      say "Skipping wire #{wire.id}: #{e.message}"
-    end
-  end
-
-  def down
-    remove_column :wires, :uetr
   end
 
 end
