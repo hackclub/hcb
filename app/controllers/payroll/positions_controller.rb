cb71d726e207d81b6adde482bffef1dcf48da4b0
Samuel Fernandez <me@sfernandez.dev>
2026-09-18 00:06:09 -0400

    Use better logic
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/controllers/payroll/positions_controller.rb
+++ app/controllers/payroll/positions_controller.rb
@@ -48,7 +48,7 @@ module Payroll
         start_date: position_params[:starts_on],
         end_date: position_params[:ends_on],
         description: position_params[:purpose],
-        combine_contract_attachment: position_params[:combine_contract_attachment].nil? ? true : position_params[:combine_contract_attachment],
+        combine_contract_attachment: position_params[:combine_contract_attachment].nil? || position_params[:combine_contract_attachment],
         manager_id: position_params[:manager_id]
       )
 
