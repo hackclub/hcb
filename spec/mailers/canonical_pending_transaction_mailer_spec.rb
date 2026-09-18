db813e94351eeeb995f1788c9e4b8a2082b35efa
httperry <priyadarshan.m@outlook.com>
2026-09-18 21:12:40 +0530

    [CPT Mailer] Check all polymorphic receiptables for charge and test Ledger::Item receipts

--- spec/mailers/canonical_pending_transaction_mailer_spec.rb
+++ spec/mailers/canonical_pending_transaction_mailer_spec.rb
@@ -32,11 +32,34 @@ RSpec.describe CanonicalPendingTransactionMailer, type: :mailer do
       end
     end
 
-    context "when a receipt has already been uploaded" do
+    context "when a receipt has already been uploaded to the HcbCode" do
       it "does not prompt to upload the receipt and clarifies it has already been uploaded" do
         hcb_code = canonical_pending_transaction.local_hcb_code
         ReceiptService::Create.new(
           receiptable: hcb_code,
+          uploader: user,
+          attachments: [file_fixture("receipt.png")],
+          upload_method: :receipts_page
+        ).run!
+
+        mail = described_class.with(
+          canonical_pending_transaction_id: canonical_pending_transaction.id,
+          canonical_transaction_id: canonical_transaction.id
+        ).notify_settled
+
+        expect(html_body(mail)).to include("A receipt has already been uploaded for this transaction.")
+        expect(html_body(mail)).to include("manage your transaction here")
+        expect(html_body(mail)).to include('alt="Receipt uploaded"')
+        expect(html_body(mail)).not_to include("Click to upload your receipt")
+        expect(html_body(mail)).not_to include("reply to this email with an image or PDF receipt attached")
+      end
+    end
+
+    context "when a receipt has already been uploaded to the Ledger::Item" do
+      it "does not prompt to upload the receipt and clarifies it has already been uploaded" do
+        ledger_item = canonical_pending_transaction.ledger_item
+        ReceiptService::Create.new(
+          receiptable: ledger_item,
           uploader: user,
           attachments: [file_fixture("receipt.png")],
           upload_method: :receipts_page
